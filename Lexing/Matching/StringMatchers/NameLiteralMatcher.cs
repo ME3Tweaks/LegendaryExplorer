0a2bd6370c75f41d5a155799e380598d40e067d7
Heff <heff@heff.se>
2015-02-24 16:28:23 +0100

    Unreal's Name type matching done for literals, with tests.

---
+++ Lexing/Matching/StringMatchers/NameLiteralMatcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ME3Script.Lexing.Matching.StringMatchers
+{
+    public class NameLiteralMatcher : TokenMatcherBase<String>
+    {
+        protected override Token<string> Match(TokenizableDataStream<string> data)
+        {
+            String value = null;
+            Regex regex = new Regex("[0-9a-zA-Z_]");
+            if (data.CurrentItem == "'")
+            {
+                data.Advance();
+                while (!data.AtEnd() && regex.IsMatch(data.CurrentItem))
+                {
+                    value += data.CurrentItem;
+                    data.Advance();
+                }
+
+                if (data.CurrentItem == "'")
+                {
+                    data.Advance();
+                    if (value == null)
+                        value = "";
+                }
+                else
+                {
+                    value = null;
+                }
+            }
+
+            return value == null ? null : new Token<String>(TokenType.String, value);
+        }
+    }
+}
