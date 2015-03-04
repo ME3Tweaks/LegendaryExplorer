acc8fd56dd92b19b2cdb1754202afbc5d47df621
Heff <heff@heff.se>
2015-03-04 17:38:54 +0100

    Began rewriting of AST/Parsing/Symbols. Current approach not feasible in the long run.

---
+++ Language/Tree/ASTNode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ME3Script.Language.Tree
+{
+    public abstract class ASTNode
+    {
+    }
+}
