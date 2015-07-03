18e664193db9bd5030ee53fa12556ae454e69589
Heff <heff@heff.se>
2015-07-03 22:14:42 +0200

    Continued work on simple conversion from pcc to ast to text.

--- Decompiling/ME3ObjectConverter.cs
+++ Decompiling/ME3ObjectConverter.cs
@@ -21,8 +21,17 @@ namespace ME3Script.Decompiling
         public Class ConvertClass()
         {
             // TODO: this is only for text decompiling, should extend to a full ast for modification.
-            var parent = new VariableType(Object.SuperField.Name, null, null);
-            var outer = new VariableType(Object.OuterClass.Name, null, null);
+            VariableType parent;
+            if (Object.SuperField != null)
+                parent = new VariableType(Object.SuperField.Name, null, null);
+            else
+                parent = new VariableType("object", null, null);
+
+            VariableType outer;
+            if (Object.OuterClass != null)
+                outer = new VariableType(Object.OuterClass.Name, null, null);
+            else
+                outer = new VariableType(parent.Name, null, null);
             // TODO: specifiers
             // TODO: operators
             // TODO: interfaces
@@ -42,31 +51,57 @@ namespace ME3Script.Decompiling
 
             var Funcs = new List<Function>();
             foreach (var member in Object.FunctionRefs)
-                Vars.Add(ConvertFunction(member));
+                Funcs.Add(ConvertFunction(member));
 
-            AST = new Class(Object.Name, null, Vars, Types, Funcs, null, 
-                parent, outer, null, null, null);
+            AST = new Class(Object.Name, new List<Specifier>(), Vars, Types, Funcs, 
+                new List<State>(), parent, outer, new List<OperatorDeclaration>(), null, null);
             return AST;
         }
 
         public Struct ConvertStruct(ME3Struct obj)
         {
-            return null;
+            var Vars = new List<VariableDeclaration>();
+            foreach (var member in obj.Variables)
+                Vars.Add(ConvertVariable(member));
+            VariableType parent = obj.SuperField != null 
+                ? new VariableType(obj.SuperField.Name, null, null) : null;
+            return new Struct(obj.Name, null, Vars, null, null, parent);
         }
 
-        public Struct ConvertEnum(ME3Enum obj)
+        public Enumeration ConvertEnum(ME3Enum obj)
         {
-            return null;
+            var vals = new List<VariableIdentifier>();
+            foreach (var val in obj.Names)
+                vals.Add(new VariableIdentifier(val, null, null));
+
+            return new Enumeration(obj.Name, vals, null, null);
         }
 
         public Variable ConvertVariable(ME3Property obj)
         {
-            return null;
+            // TODO: fix
+            var type = new VariableType(obj.ExportEntry.ClassName, null, null);
+            return new Variable(new List<Specifier>(), 
+                new VariableIdentifier(obj.Name, null, null), type, null, null);
         }
 
-        public Variable ConvertFunction(ME3Function obj)
+        public Function ConvertFunction(ME3Function obj)
         {
-            return null;
+            VariableType returnType = null;
+            if (obj.ReturnValue != null)
+                returnType = new VariableType(obj.ReturnValue.Name, null, null);
+            var parameters = new List<FunctionParameter>();
+            foreach(var param in obj.Parameters)
+            {
+                var convert = ConvertVariable(param);
+                parameters.Add(new FunctionParameter(convert.VarType, 
+                    convert.Specifiers, convert.Variables.First(), 
+                    null, null));
+            }
+
+            return new Function(obj.Name, returnType, 
+                new CodeBody(new List<Statement>(), null, null),
+                new List<Specifier>(), parameters, null, null);
         }
     }
 }
