87c3ca1aeb8804b1ce7bbb1f7f73634e9c87c5e0
Heff <heff@heff.se>
2015-07-04 15:40:53 +0200

    Began work on bytecode decompiling. Went through known bytecodes and tried to match with IDA, a lot of unknowns still.

---
+++ Decompiling/ME3ByteCodeDecompiler.cs
@@ -0,0 +1,47 @@
+﻿using ME3Data.DataTypes.ScriptTypes;
+using ME3Script.Language.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ME3Script.Decompiling
+{
+    public class ME3ByteCodeDecompiler
+    {
+        private ME3Struct DataContainer;
+
+        private Byte[] Data { get { return DataContainer.DataScript; } }
+        private int Index;
+
+        private Byte CurrentByte { get { return Data[Index]; } }
+        private Byte PopByte { get { return Data[Index++]; } }
+        private Byte PeekByte { get { return Data[Index + 1]; } }
+        private Byte PrevByte { get { return Data[Index - 1]; } }
+
+        private bool CurrentIs()
+
+        public ME3ByteCodeDecompiler(ME3Struct dataContainer)
+        {
+            DataContainer = dataContainer;
+        }
+
+        public CodeBody Decompile()
+        {
+            Index = 0;
+
+            while (Index < Data.Length) //&& CurrentByte)
+            {
+                
+            }
+
+            return null;
+        }
+
+        public Statement DecompileStatement()
+        {
+
+        }
+    }
+}
---
+++ Language/ByteCode/Standard.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ME3Script.Language.ByteCode
+{
+    public enum StandardByteCodes
+    {
+        LocalVariable = 0x00,
+        InstanceVariable = 0x01,
+        DefaultVariable = 0x02,
+        Return = 0x04,
+        Switch = 0x05,
+        Jump = 0x06,
+        JumpIfNot = 0x07,
+        Stop = 0x08,
+        Assert = 0x09,
+        Case = 0x0A,
+        Nothing = 0x0B,
+        LabelTable = 0x0C,
+        GotoLabel = 0x0D,
+        EatReturnValue = 0x0E,
+        Let = 0x0F,
+        DynArrayElement = 0x10,
+        New = 0x11,
+        ClassContext = 0x12,
+        Metacast = 0x13,
+        LetBool = 0x14,
+        EndParmValue = 0x15,
+        EndFunctionParms = 0x16,
+        Self = 0x17,
+        Skip = 0x18,
+        Context = 0x19,
+        ArrayElement = 0x1A,
+        VirtualFunction = 0x1B,
+        FinalFunction = 0x1C,
+        IntConst = 0x1D,
+        FloatConst = 0x1E,
+        StringConst = 0x1F,
+        ObjectConst = 0x20,
+        NameConst = 0x21,
+        RotationConst = 0x22,
+        VectorConst = 0x23,
+        ByteConst = 0x24,
+        IntZero = 0x25,
+        IntOne = 0x26,
+        True = 0x27,
+        False = 0x28,
+        NativeParm = 0x29,
+        NoObject = 0x2A,
+        IntConstByte = 0x2C,
+        BoolVariable = 0x2D,
+        DynamicCast = 0x2E,
+        Iterator = 0x2F,
+        IteratorPop = 0x30,
+        IteratorNext = 0x31,
+        StructCmpEq = 0x32,
+        StructCmpNe = 0x33,
+        UnicodeStringConst = 0x34,
+        StructMember = 0x35,
+        DynArrayLength = 0x36,
+        GlobalFunction = 0x37,
+        PrimitiveCast = 0x38,
+        DynArrayInsert = 0x39,
+        ByteToInt = 0x3A,        // TODO: ReturnNothing = 0x3A old or new?
+        EqualEqual_DelDel = 0x3B,
+        NotEqual_DelDel = 0x3C,
+        EqualEqual_DelFunc = 0x3D,
+        NotEqual_DelFunc = 0x3E,
+        EmptyDelegate = 0x3F,
+        DynArrayRemove = 0x40,
+        DebugInfo = 0x41,
+        DelegateFunction = 0x42,
+        DelegateProperty = 0x43,
+        LetDelegate = 0x44,
+        Conditional = 0x45,
+        DynArrayFind = 0x46,
+        DynArrayFindStruct = 0x47,
+        LocalOutVariable = 0x48,
+        DefaultParmValue = 0x49,
+        EmptyParmValue = 0x4A,
+        InstanceDelegate = 0x4B,
+        // 0x4C - 0x4E are unknown, probably invalid, throws assert failure by the looks of it
+        Unkn_4F = 0x4F,
+        GoW_DefaultValue = 0x50,
+        InterfaceContext = 0x51,
+        InterfaceCast = 0x52,
+        EndOfScript = 0x53,
+        DynArrayAdd = 0x54,
+        DynArrayAddItem = 0x55,
+        DynArrayRemoveItem = 0x56,
+        DynArrayInsertItem = 0x57,
+        DynArrayIterator = 0x58,
+        DynArrSort = 0x59,
+        Unkn_5A = 0x5A, // FilterEditorOnly?
+        Unkn_5B = 0x5B, // 0-2 , 48, 5B-62, all share the same behavior
+        Unkn_5C = 0x5C, // presumably variable related?
+        Unkn_5D = 0x5D,
+        Unkn_5E = 0x5E,
+        Unkn_5F = 0x5F,
+        Unkn_60 = 0x60,
+        Unkn_61 = 0x61,
+        Unkn_62 = 0x62,
+        Unkn_63 = 0x63,
+        Unkn_64 = 0x64,
+        Unkn_65 = 0x65,
+        // 66-6F are unknown, probably invalid, throws assert failure by the looks of it
+
+
+        // 63 and 64 also share
+
+        // 0x00 - 0x6F seem to be standard
+        // 0x70 - 0x7F seem to be extended natives
+        // 0x80 - 0xFF likely to be natives
+
+        // 3B - 3F seem to be natives as well, UE3 standard is delegate comparison operations
+        // seemingly does not extract the second halfbyte of the instruction, weird.
+    }
+}
