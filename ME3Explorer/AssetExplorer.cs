ME3Tweaks/LegendaryExplorer

commit 41857bde5e086964996a946ea39be428c64fee26

Prevent crashing if pcc can't be opened
Resolves #45

ME3Explorer/AssetExplorer.cs
@@ -122,11 +122,18 @@ namespace ME3Explorer
 
         public void LoadFile(string s)
         {
-            if (!File.Exists(s))
-                return;
-            currentPCC = s;
-            pcc = new PCCObject(s);
-            GeneratePccTree();
+            try
+            {
+                if (!File.Exists(s))
+                    return;
+                pcc = new PCCObject(s);
+                currentPCC = s;
+                GeneratePccTree();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:\n" + ex.Message);
+            }
         }
 
         public void GeneratePccTree()
@@ -215,22 +222,29 @@ namespace ME3Explorer
             Println("Loading " + t.Text + " ...");
 
             string DLCName = t.Parent.Text;
-            if (DLCName == mainPCCFolder)
+            try
             {
-                currentPCC = ME3Directory.cookedPath + t.Text;
-                pcc = new PCCObject(currentPCC);
-            }
-            else
-            {
-                currentPCC = t.Name;
-                string tempPCCPath = Path.GetFileName(currentPCC);
-                currentDLC = new AmaroK86.MassEffect3.DLCBase(ME3Directory.DLCFilePath(DLCName));
-                currentDLC.extractFile(currentPCC, tempPCCPath);
-                pcc = new PCCObject(tempPCCPath);
-                pcc.bDLCStored = true;
-            }
+                if (DLCName == mainPCCFolder)
+                {
+                    pcc = new PCCObject(currentPCC);
+                    currentPCC = ME3Directory.cookedPath + t.Text;
+                }
+                else
+                {
+                    currentPCC = t.Name;
+                    string tempPCCPath = Path.GetFileName(currentPCC);
+                    currentDLC = new AmaroK86.MassEffect3.DLCBase(ME3Directory.DLCFilePath(DLCName));
+                    currentDLC.extractFile(currentPCC, tempPCCPath);
+                    pcc = new PCCObject(tempPCCPath);
+                    pcc.bDLCStored = true;
+                }
 
-            GeneratePccTree();
+                GeneratePccTree();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:\n" + ex.Message);
+            }
         }
 
         private void TV1_AfterSelect(object sender, TreeViewEventArgs e)
@@ -1125,11 +1139,18 @@ namespace ME3Explorer
             p.MdiParent = this.MdiParent;
             p.WindowState = FormWindowState.Maximized;
             p.Show();
-            p.pcc = new PCCObject(currentPCC);
-            p.SetView(2);
-            p.RefreshView();
-            p.InitStuff();
-            p.listBox1.SelectedIndex = l;
+            try
+            {
+                p.pcc = new PCCObject(currentPCC);
+                p.SetView(2);
+                p.RefreshView();
+                p.InitStuff();
+                p.listBox1.SelectedIndex = l;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:\n" + ex.Message);
+            }
         }
 
         private void AssetExplorer_FormClosing(object sender, FormClosingEventArgs e)

ME3Explorer/AnimationExplorer/AnimationExplorer.cs
@@ -36,24 +36,31 @@ namespace ME3Explorer.AnimationExplorer
 
         public void LoadPcc(string s)
         {
-            AT = new List<AnimTree>();
-            AS = new List<AnimSet>();
-            pcc = new PCCObject(s);
-            for (int i = 0; i < pcc.Exports.Count; i++)
-                switch(pcc.Exports[i].ClassName)
-                {
-                    case "AnimTree":
-                        AT.Add(new AnimTree(pcc, i));
-                        break;
-                    case "AnimSet":
-                        AS.Add(new AnimSet(pcc, i));
-                        break;
-                }
-            treeView1.Nodes.Clear();
-            foreach (AnimTree at in AT)
-                treeView1.Nodes.Add(at.ToTree());
-            foreach(AnimSet ans in AS)
-                treeView1.Nodes.Add(ans.ToTree());
+            try
+            {
+                pcc = new PCCObject(s);
+                AT = new List<AnimTree>();
+                AS = new List<AnimSet>();
+                for (int i = 0; i < pcc.Exports.Count; i++)
+                    switch (pcc.Exports[i].ClassName)
+                    {
+                        case "AnimTree":
+                            AT.Add(new AnimTree(pcc, i));
+                            break;
+                        case "AnimSet":
+                            AS.Add(new AnimSet(pcc, i));
+                            break;
+                    }
+                treeView1.Nodes.Clear();
+                foreach (AnimTree at in AT)
+                    treeView1.Nodes.Add(at.ToTree());
+                foreach (AnimSet ans in AS)
+                    treeView1.Nodes.Add(ans.ToTree());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:\n" + ex.Message);
+            }
         }
 
         private void startScanToolStripMenuItem_Click(object sender, EventArgs e)
@@ -69,18 +76,26 @@ namespace ME3Explorer.AnimationExplorer
             int count = 1;
             foreach (string file in files)
             {
-                PCCObject _pcc = new PCCObject(file);
-                DebugOutput.PrintLn((count++) + "/" + files.Length + " : Scanning file " + Path.GetFileName(file) + " ...");
-                bool found = false;
-                foreach(PCCObject.ExportEntry ex in _pcc.Exports)
-                    if (ex.ClassName == "AnimTree" || ex.ClassName == "AnimSet")
-                    {
-                        DebugOutput.PrintLn("Found Animation!");
-                        found = true;
-                        break;
-                    }
-                if(found)
-                    filenames.Add(file);
+                try
+                {
+                    PCCObject _pcc = new PCCObject(file);
+                    DebugOutput.PrintLn((count++) + "/" + files.Length + " : Scanning file " + Path.GetFileName(file) + " ...");
+                    bool found = false;
+                    foreach (PCCObject.ExportEntry ex in _pcc.Exports)
+                        if (ex.ClassName == "AnimTree" || ex.ClassName == "AnimSet")
+                        {
+                            DebugOutput.PrintLn("Found Animation!");
+                            found = true;
+                            break;
+                        }
+                    if (found)
+                        filenames.Add(file);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:\n" + ex.Message);
+                    DebugOutput.PrintLn("Could not open " + Path.GetFileName(file));
+                }
             }
             RefreshLists();
         }

ME3Explorer/ClassViewer/ClassViewer.cs
@@ -28,12 +28,19 @@ namespace ME3Explorer.ClassViewer
             d.Filter = "*.pcc|*.pcc";
             if (d.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                pcc = new PCCObject(d.FileName);
-                Objects = new List<int>();
-                for (int i = 0; i < pcc.Exports.Count; i++)
-                    if (pcc.Exports[i].ClassName == "Class")
-                        Objects.Add(i);
-                RefreshLists();
+                try
+                {
+                    pcc = new PCCObject(d.FileName);
+                    Objects = new List<int>();
+                    for (int i = 0; i < pcc.Exports.Count; i++)
+                        if (pcc.Exports[i].ClassName == "Class")
+                            Objects.Add(i);
+                    RefreshLists();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:\n" + ex.Message);
+                }
             }
         }
 

ME3Explorer/Codex Editor/CodexEditor.cs
@@ -44,7 +44,16 @@ namespace ME3Explorer.Codex_Editor
                 this.Close();
                 return;
             }
-            pcc = new PCCObject(pathcook + "SFXGameInfoSP_SF.pcc");
+            try
+            {
+                pcc = new PCCObject(pathcook + "SFXGameInfoSP_SF.pcc");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:\n" + ex.Message);
+                this.Close();
+                return;
+            }
             GetEntries();
             RefreshTree();
         }

ME3Explorer/DialogEditor/DialogEditor.cs
@@ -50,13 +50,20 @@ namespace ME3Explorer.DialogEditor
             d.Filter = "*.pcc|*.pcc";
             if (d.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                InitTalkFile(talkFile);
-                pcc = new PCCObject(d.FileName);
-                Objs = new List<int>();
-                for (int i = 0; i < pcc.Exports.Count; i++)
-                    if (pcc.Exports[i].ClassName == "BioConversation")
-                        Objs.Add(i);
-                RefreshCombo();
+                try
+                {
+                    pcc = new PCCObject(d.FileName);
+                    InitTalkFile(talkFile);
+                    Objs = new List<int>();
+                    for (int i = 0; i < pcc.Exports.Count; i++)
+                        if (pcc.Exports[i].ClassName == "BioConversation")
+                            Objs.Add(i);
+                    RefreshCombo();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:\n" + ex.Message);
+                }
             }
         }
 

ME3Explorer/FaceFXAnimSetEditor/FaceFXAnimSetEditor.cs
@@ -29,12 +29,19 @@ namespace ME3Explorer.FaceFXAnimSetEditor
             d.Filter = "*.pcc|*.pcc";
             if (d.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                Objects = new List<int>();
-                pcc = new PCCObject(d.FileName);
-                for (int i = 0; i < pcc.Exports.Count; i++)
-                    if (pcc.Exports[i].ClassName == "FaceFXAnimSet")
-                        Objects.Add(i);
-                ListRefresh();
+                try
+                {
+                    pcc = new PCCObject(d.FileName);
+                    Objects = new List<int>();
+                    for (int i = 0; i < pcc.Exports.Count; i++)
+                        if (pcc.Exports[i].ClassName == "FaceFXAnimSet")
+                            Objects.Add(i);
+                    ListRefresh();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:\n" + ex.Message);
+                }
             }
         }
 

ME3Explorer/GUIDCacheEditor/GUIDCacheEditor.cs
@@ -38,9 +38,16 @@ namespace ME3Explorer.GUIDCacheEditor
                 return;
             }
             BitConverter.IsLittleEndian = true;
-            pcc = new PCCObject(ME3Directory.cookedPath + "GuidCache.pcc");
-            ReadGUIDs(pcc.Exports[0].Data);
-            RefreshLists();
+            try
+            {
+                pcc = new PCCObject(ME3Directory.cookedPath + "GuidCache.pcc");
+                ReadGUIDs(pcc.Exports[0].Data);
+                RefreshLists();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:\n" + ex.Message);
+            }
         }
 
         public void ReadGUIDs(byte[] buff)

ME3Explorer/InterpEditor/InterpEditor.cs
@@ -60,21 +60,28 @@ namespace ME3Explorer.InterpEditor
 
         public void LoadPCC(string fileName, Object editorTalkFile = null)
         {
-            if (editorTalkFile != null)
+            try
             {
-                InitTalkFile(editorTalkFile);
+                pcc = new PCCObject(fileName);
+                if (editorTalkFile != null)
+                {
+                    InitTalkFile(editorTalkFile);
+                }
+                else
+                {
+                    InitTalkFile(talkfile);
+                }
+                objects.Clear();
+                CurrentFile = fileName;
+                for (int i = 0; i < pcc.Exports.Count; i++)
+                    if (pcc.Exports[i].ClassName == "InterpData")
+                        objects.Add(i);
+                RefreshCombo();
             }
-            else
+            catch (Exception ex)
             {
-                InitTalkFile(talkfile);
+                MessageBox.Show("Error:\n" + ex.Message);
             }
-            objects.Clear();
-            pcc = new PCCObject(fileName);
-            CurrentFile = fileName;
-            for (int i = 0; i < pcc.Exports.Count; i++)
-                if (pcc.Exports[i].ClassName == "InterpData")
-                    objects.Add(i);
-            RefreshCombo();
         }
 
         public void RefreshCombo()

commit 052b51df44406d8ea25fd308a7e2fa781c578082

Asset Explorer: fix bug that prevents opening pcc from global tree
bug introduced by 41857bd

ME3Explorer/AssetExplorer.cs
@@ -226,8 +226,8 @@ namespace ME3Explorer
             {
                 if (DLCName == mainPCCFolder)
                 {
-                    pcc = new PCCObject(currentPCC);
                     currentPCC = ME3Directory.cookedPath + t.Text;
+                    pcc = new PCCObject(currentPCC);
                 }
                 else
                 {