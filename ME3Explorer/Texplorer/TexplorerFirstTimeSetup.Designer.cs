d4b267507f56b417f5837715333a28c411fb65ec
SirCxyrtyx <sircxyrtyx@gmail.com>
2018-04-13 11:42:48 -0700

    Added deselect/select all button to Texplorer dlc selection

--- ME3Explorer/Texplorer/TexplorerFirstTimeSetup.Designer.cs
+++ ME3Explorer/Texplorer/TexplorerFirstTimeSetup.Designer.cs
@@ -36,7 +36,12 @@
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.StatusProgLabel = new System.Windows.Forms.ToolStripLabel();
             this.MainListView = new System.Windows.Forms.CheckedListBox();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.deselectAllButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.selectAllButton = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -50,10 +55,9 @@
             this.StatusProgBar,
             this.toolStripSeparator1,
             this.StatusProgLabel});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 557);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 370);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
-            this.toolStrip1.Size = new System.Drawing.Size(568, 37);
+            this.toolStrip1.Size = new System.Drawing.Size(379, 26);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -63,7 +67,7 @@
             this.ContinueButton.Image = ((System.Drawing.Image)(resources.GetObject("ContinueButton.Image")));
             this.ContinueButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ContinueButton.Name = "ContinueButton";
-            this.ContinueButton.Size = new System.Drawing.Size(87, 34);
+            this.ContinueButton.Size = new System.Drawing.Size(60, 23);
             this.ContinueButton.Text = "Continue";
             this.ContinueButton.Click += new System.EventHandler(this.ContinueButton_Click);
             // 
@@ -74,24 +78,24 @@
             this.ExitButton.Image = ((System.Drawing.Image)(resources.GetObject("ExitButton.Image")));
             this.ExitButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(67, 34);
+            this.ExitButton.Size = new System.Drawing.Size(47, 23);
             this.ExitButton.Text = "Cancel";
             this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
             // StatusProgBar
             // 
             this.StatusProgBar.Name = "StatusProgBar";
-            this.StatusProgBar.Size = new System.Drawing.Size(150, 34);
+            this.StatusProgBar.Size = new System.Drawing.Size(100, 23);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 37);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 26);
             // 
             // StatusProgLabel
             // 
             this.StatusProgLabel.Name = "StatusProgLabel";
-            this.StatusProgLabel.Size = new System.Drawing.Size(131, 34);
+            this.StatusProgLabel.Size = new System.Drawing.Size(86, 23);
             this.StatusProgLabel.Text = "toolStripLabel1";
             // 
             // MainListView
@@ -100,27 +104,65 @@
             this.MainListView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MainListView.FormattingEnabled = true;
             this.MainListView.HorizontalScrollbar = true;
-            this.MainListView.Location = new System.Drawing.Point(0, 0);
-            this.MainListView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MainListView.Location = new System.Drawing.Point(0, 25);
             this.MainListView.Name = "MainListView";
-            this.MainListView.Size = new System.Drawing.Size(568, 557);
+            this.MainListView.Size = new System.Drawing.Size(379, 345);
             this.MainListView.TabIndex = 0;
             this.MainListView.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.MainListView_ItemCheck);
             this.MainListView.SelectedIndexChanged += new System.EventHandler(this.MainListView_SelectedIndexChanged);
             // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deselectAllButton,
+            this.toolStripSeparator2,
+            this.selectAllButton});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(379, 25);
+            this.toolStrip2.TabIndex = 1;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // deselectAllButton
+            // 
+            this.deselectAllButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.deselectAllButton.Image = ((System.Drawing.Image)(resources.GetObject("deselectAllButton.Image")));
+            this.deselectAllButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.deselectAllButton.Name = "deselectAllButton";
+            this.deselectAllButton.Size = new System.Drawing.Size(72, 22);
+            this.deselectAllButton.Text = "Deselect All";
+            this.deselectAllButton.Click += new System.EventHandler(this.deselectAllButton_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // selectAllButton
+            // 
+            this.selectAllButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.selectAllButton.Image = ((System.Drawing.Image)(resources.GetObject("selectAllButton.Image")));
+            this.selectAllButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.selectAllButton.Name = "selectAllButton";
+            this.selectAllButton.Size = new System.Drawing.Size(59, 22);
+            this.selectAllButton.Text = "Select All";
+            this.selectAllButton.Click += new System.EventHandler(this.selectAllButton_Click);
+            // 
             // TexplorerFirstTimeSetup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(568, 594);
+            this.ClientSize = new System.Drawing.Size(379, 396);
             this.Controls.Add(this.MainListView);
+            this.Controls.Add(this.toolStrip2);
             this.Controls.Add(this.toolStrip1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "TexplorerFirstTimeSetup";
             this.Text = "TexplorerFirstTimeSetup";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormClosingEvent);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,5 +177,9 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripLabel StatusProgLabel;
         private System.Windows.Forms.CheckedListBox MainListView;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton deselectAllButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton selectAllButton;
     }
 }
