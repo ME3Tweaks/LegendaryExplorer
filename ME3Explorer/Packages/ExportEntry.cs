1d342c244c21a73c160d8e55654570e59e2df6c5
Mgamerz <mgamerz2@gmail.com>
2018-09-09 01:33:15 -0600

    CurveEditor: Fix bug that would write an additional none property when modifying InterpTrackFloatProp curves. This would cause the final property to be no longer parsed or shown as well as adding an additional none very time the point moved while you were dragging it.

--- ME3Explorer/Packages/ExportEntry.cs
+++ ME3Explorer/Packages/ExportEntry.cs
@@ -215,10 +215,19 @@ namespace ME3Explorer.Packages
 
             int propStart = GetPropertyStart();
             int propEnd = propsEnd();
-            //Debug.WriteLine("Datasize before write: " + this.Data.Length);
-            this.Data = _data.Take(propStart).Concat(m.ToArray()).Concat(_data.Skip(propEnd)).ToArray();
-            //Debug.WriteLine("Datasize after write: " + this.Data.Length);
+            byte[] propData = m.ToArray();
 
+            /*            Debug.WriteLine("Datasize before write: " + this.Data.Length);
+            var newData = _data.Take(propStart);
+            var data1 = newData.ToArray();
+            newData = newData.Concat(propData);
+            var data2 = newData.ToArray();
+            newData = newData.Concat(_data.Skip(propEnd));
+            var data3 = newData.ToArray();
+            */
+            this.Data = _data.Take(propStart).Concat(propData).Concat(_data.Skip(propEnd)).ToArray();
+
+            //Debug.WriteLine("Datasize after write: " + this.Data.Length);
         }
 
         public void WriteProperty(UProperty prop)
