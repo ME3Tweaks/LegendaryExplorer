2a8cde351e14c0db6eb9684675a1496362c6815f
Mgamerz <mgamerz2@gmail.com>
2018-10-01 21:22:23 -0600

    Configure DLCUnpacker for MEM code implementation

--- ME3Explorer/DLCUnpacker/DLCUnpacker.xaml.cs
+++ ME3Explorer/DLCUnpacker/DLCUnpacker.xaml.cs
@@ -5,9 +5,11 @@ using ME3Explorer.Unreal;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,17 +25,115 @@ namespace ME3Explorer.DLCUnpacker
     /// <summary>
     /// Interaction logic for DLCUnpacker.xaml
     /// </summary>
-    public partial class DLCUnpacker : WPFBase, INotifyPropertyChanged
+    public partial class DLCUnpacker : WPFBase
     {
+        public ICommand UnpackDLCCommand { get; set; }
+        private BackgroundWorker UnpackDLCWorker;
+        private double RequiredSpace;
+        private double AvailableSpace;
 
-        public string UnpackingPercentString { get; set; }
-        public string RequiredSpaceText { get; set; }
-        public string AvailableSpaceText { get; set; }
+        #region MVVM Databindings
+        private string _unpackingPercentString;
+        public string UnpackingPercentString
+        {
+            get { return _unpackingPercentString; }
+            set
+            {
+                if (value != _unpackingPercentString)
+                {
+                    _unpackingPercentString = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _requiredSpaceText;
+        public string RequiredSpaceText
+        {
+            get { return _requiredSpaceText; }
+            set
+            {
+                if (value != _requiredSpaceText)
+                {
+                    _requiredSpaceText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _availableSpaceText;
+        public string AvailableSpaceText
+        {
+            get { return _availableSpaceText; }
+            set
+            {
+                if (value != _availableSpaceText)
+                {
+                    _availableSpaceText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private double _currentOverallProgressValue;
+        public double CurrentOverallProgressValue
+        {
+            get { return _currentOverallProgressValue; }
+            set
+            {
+                if (value != _currentOverallProgressValue)
+                {
+                    _currentOverallProgressValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
+        //Used for the current operation (e.g. which DLC is being unpacked, it's %)
+        private int _currentOperationProgressValue;
+        public int CurrentOperationPercentValue
+        {
+            get { return _currentOperationProgressValue; }
+            set
+            {
+                if (value != _currentOperationProgressValue)
+                {
+                    _currentOperationProgressValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool _progressBarIndeterminate;
+        public bool ProgressBarIndeterminate
+        {
+            get { return _progressBarIndeterminate; }
+            set
+            {
+                if (value != _progressBarIndeterminate)
+                {
+                    _progressBarIndeterminate = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _currentOperationText;
+        public string CurrentOperationText
+        {
+            get { return _currentOperationText; }
+            set
+            {
+                if (value != _currentOperationText)
+                {
+                    _currentOperationText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
         List<DLCPackage> sfarsToUnpack = new List<DLCPackage>();
 
         public DLCUnpacker()
         {
+            LoadCommands();
+            RequiredSpaceText = "Calculating...";
             InitializeComponent();
             BackgroundWorker bg = new BackgroundWorker();
             bg.DoWork += CalculateUnpackRequirements;
@@ -45,45 +145,40 @@ namespace ME3Explorer.DLCUnpacker
 
         private void CalculateUnpackRequirements_Completed(object sender, RunWorkerCompletedEventArgs e)
         {
-            throw new NotImplementedException();
+            CommandManager.InvalidateRequerySuggested(); //Refresh commands
         }
 
         private void CalculateUnpackRequirements(object sender, DoWorkEventArgs e)
         {
+            //Background thread
+            CalculateUnpackRequirements();
+        }
+
+        /// <summary>
+        /// Calculates the space required and available space for unpacking.
+        /// If there is nothing to unpack (0 bytes to unpack) the unpack DLC button will become locked.
+        /// </summary>
+        private void CalculateUnpackRequirements()
+        {
             if (ME3Directory.gamePath != null)
             {
+                ProgressBarIndeterminate = true;
                 var parts = ME3Directory.gamePath.Split(':');
                 DriveInfo info = new DriveInfo(parts[0]);
-                double availableSpace = info.AvailableFreeSpace;
-                AvailableSpaceText = ByteSize.FromBytes(availableSpace).ToString();
-
-                double requiredSpace = GetRequiredSize();
-                if (requiredSpace >= 0)
+                AvailableSpace = info.AvailableFreeSpace;
+                AvailableSpaceText = ByteSize.FromBytes(AvailableSpace).ToString();
+                Debug.WriteLine("Available space set");
+                RequiredSpace = GetRequiredSize();
+                if (RequiredSpace >= 0)
                 {
-                    RequiredSpaceText = ByteSize.FromBytes(requiredSpace).ToString();
+                    RequiredSpaceText = ByteSize.FromBytes(RequiredSpace).ToString();
                 }
                 else
                 {
-                    //requiredSpaceRun.Text = "?";
+                    RequiredSpaceText = "Error calculating";
                 }
-
-                /*unpackOutput.AppendLine($"Packed DLC detected: {sfarsToUnpack.Count}");*/
-                if (sfarsToUnpack.Count > 0)
-                {
-                    if (availableSpace < requiredSpace)
-                    {
-                        //requiredSpaceRun.Foreground = new SolidColorBrush(Color.FromRgb(0xb8, 0, 0));
-
-                    }
-                }
-                else
-                {
-                }
+                ProgressBarIndeterminate = false;
             }
-        }
-        private void prepDLCUnpacking()
-        {
-            
         }
 
         private double GetRequiredSize()
@@ -100,7 +195,7 @@ namespace ME3Explorer.DLCUnpacker
             double temp;
             foreach (var folder in unextracted)
             {
-                if (folder.Contains("__metadata"))
+                if (!System.IO.Path.GetFileName(folder).StartsWith("DLC"))
                     continue;
 
                 try
@@ -108,6 +203,7 @@ namespace ME3Explorer.DLCUnpacker
                     FileInfo info = new FileInfo(Directory.EnumerateFiles(folder, "*", SearchOption.AllDirectories).First(file => file.EndsWith(".sfar", StringComparison.OrdinalIgnoreCase)));
                     compressedSize += info.Length;
 
+                    //This can be changed to MEM code
                     DLCPackage sfar = new DLCPackage(info.FullName);
                     sfarsToUnpack.Add(sfar);
 
@@ -124,15 +220,47 @@ namespace ME3Explorer.DLCUnpacker
                 }
             }
             totalUncompressedSize = uncompressedSize;
+
+            //This can probably be changed (original code comment is as follows)
+
             //each SFAR is stripped of all its files after unpacking, so the maximum space needed on the drive is 
             //the difference between the uncompressed size and compressed size of all SFARS, plus the compressed size
             //of the largest SFAR. I'm using the uncompressed size instead as a fudge factor.
             return (uncompressedSize - compressedSize) + largestUncompressedSize;
         }
 
-        private void UnpackDLCButton_Click(object sender, RoutedEventArgs e)
+        private void LoadCommands()
         {
+            // Player commands
+            UnpackDLCCommand = new RelayCommand(UnpackDLC, CanUnpackDLC);
+        }
 
+        private bool CanUnpackDLC(object obj)
+        {
+            return AvailableSpace > RequiredSpace && RequiredSpace > 0 && UnpackDLCWorker == null;
+        }
+
+        private void UnpackDLC(object obj)
+        {
+            UnpackDLCWorker = new BackgroundWorker();
+            UnpackDLCWorker.DoWork += UnpackAllDLC;
+            UnpackDLCWorker.RunWorkerCompleted += UnpackAllDLC_Completed;
+            UnpackDLCWorker.RunWorkerAsync();
+        }
+
+        private void UnpackAllDLC_Completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //Unpack DLC and update binded values as appropriate, if any.
+            CommandManager.InvalidateRequerySuggested(); //Refresh commands
+            UnpackDLCWorker = null;
+        }
+
+        private void UnpackAllDLC(object sender, DoWorkEventArgs e)
+        {
+            //Post completion stuff here
+
+            // ... 
+            CalculateUnpackRequirements(); //Will lock the unpack button and update UI for user
         }
 
         public override void handleUpdate(List<PackageUpdate> updates)
