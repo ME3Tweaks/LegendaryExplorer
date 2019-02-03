9f3d4ec40469f744b7cd2305aca136a31523fb55
Mgamerz <mgamerz2@gmail.com>
2019-02-03 10:32:51 -0700

    Sync commit - Add TLKManagerWPF window

---
+++ ME3Explorer/TlkManager/TLKManagerWPF.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ME3Explorer.TlkManager
+{
+    /// <summary>
+    /// Interaction logic for TLKManagerWPF.xaml
+    /// </summary>
+    public partial class TLKManagerWPF : Window
+    {
+        public TLKManagerWPF()
+        {
+            InitializeComponent();
+        }
+    }
+}
