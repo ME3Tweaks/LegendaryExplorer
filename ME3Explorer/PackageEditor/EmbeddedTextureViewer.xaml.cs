c0e71693694b39371a4d92e6581c87c0a740dccc
Mgamerz <mgamerz2@gmail.com>
2019-04-02 22:58:29 -0600

    EmbeddedTextureViewer: Support reading uncompressed textures. Trying to work on compressed, not working yet

--- ME3Explorer/PackageEditor/EmbeddedTextureViewer.xaml.cs
+++ ME3Explorer/PackageEditor/EmbeddedTextureViewer.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using Gammtek.Conduit.Extensions.IO;
 using KFreonLib.Textures;
 using ME3Explorer.Packages;
+using ME3Explorer.SharedUI;
 using ME3Explorer.Unreal;
 using static ME3Explorer.BinaryInterpreter;
 
@@ -25,8 +26,13 @@ namespace ME3Explorer
     /// </summary>
     public partial class EmbeddedTextureViewer : ExportLoaderControl
     {
+        public ObservableCollectionExtended<Texture2DMipInfo> MipList { get; private set; } = new ObservableCollectionExtended<Texture2DMipInfo>();
+        private string CurrentLoadedFormat;
+        private string CurrentLoadedCacheName;
+
         public EmbeddedTextureViewer()
         {
+            DataContext = this;
             InitializeComponent();
         }
 
@@ -37,60 +43,91 @@ namespace ME3Explorer
 
         public override void LoadExport(IExportEntry exportEntry)
         {
-            PropertyCollection properties = exportEntry.GetProperties();
-            var format = properties.GetProp<EnumProperty>("Format");
-            TextPrev.Content = format.Value;
-
-            MemoryStream ms = new MemoryStream(exportEntry.Data);
-            ms.Seek(properties.endOffset, SeekOrigin.Begin);
-            List<Texture2DMipInfo> mips = new List<Texture2DMipInfo>();
-            int numMipMaps = ms.ReadInt32();
-            for (int l = 0; l < numMipMaps; l++)
+            try
             {
-                Texture2DMipInfo mip = new Texture2DMipInfo();
-                mip.storageType = (StorageTypes)ms.ReadInt32();
-                mip.uncompressedSize = ms.ReadInt32();
-                mip.compressedSize = ms.ReadInt32();
-                mip.offset = ms.ReadInt32();
-                switch (mip.storageType)
+                PropertyCollection properties = exportEntry.GetProperties();
+                var format = properties.GetProp<EnumProperty>("Format");
+                var cache = properties.GetProp<NameProperty>("TextureFileCacheName");
+                if (cache != null)
                 {
-                    case StorageTypes.pccUnc:
-                        ms.Seek(mip.uncompressedSize, SeekOrigin.Current);
-                        break;
-                    case StorageTypes.pccLZO:
-                    case StorageTypes.pccZlib:
-                        ms.Seek(mip.compressedSize, SeekOrigin.Current);
-                        break;
+                    CurrentLoadedCacheName = cache.Value.Name;
                 }
-                mip.width = ms.ReadInt32();
-                mip.height = ms.ReadInt32();
-                mips.Add(mip);
-            }
+                MemoryStream ms = new MemoryStream(exportEntry.Data);
+                ms.Seek(properties.endOffset, SeekOrigin.Begin);
+                List<Texture2DMipInfo> mips = new List<Texture2DMipInfo>();
+                int numMipMaps = ms.ReadInt32();
+                for (int l = 0; l < numMipMaps; l++)
+                {
+                    Texture2DMipInfo mip = new Texture2DMipInfo();
+                    mip.index = l;
+                    mip.storageType = (StorageTypes)ms.ReadInt32();
+                    mip.uncompressedSize = ms.ReadInt32();
+                    mip.compressedSize = ms.ReadInt32();
+                    mip.offset = ms.ReadInt32();
+                    switch (mip.storageType)
+                    {
+                        case StorageTypes.pccUnc:
+                            ms.Seek(mip.uncompressedSize, SeekOrigin.Current);
+                            break;
+                        case StorageTypes.pccLZO:
+                        case StorageTypes.pccZlib:
+                            ms.Seek(mip.compressedSize, SeekOrigin.Current);
+                            break;
+                    }
+                    mip.width = ms.ReadInt32();
+                    mip.height = ms.ReadInt32();
+                    mips.Add(mip);
+                }
 
-            string content = "";
-            foreach (Texture2DMipInfo mip in mips)
+
+                var topmip = mips.FirstOrDefault(x => x.storageType != StorageTypes.empty);
+
+                CurrentLoadedExport = exportEntry;
+                CurrentLoadedFormat = format.Value.Name;
+                MipList.ReplaceAll(mips);
+                LoadMip(topmip);
+            }
+            catch (Exception e)
             {
-                content += "----\n";
-                content += mip.storageType;
-                content += "\n";
-                content += mip.uncompressedSize;
-                content += "\n";
-                content += mip.compressedSize;
-                content += "\n";
-                content += mip.offset;
-                content += "\n";
-                content += mip.width;
-                content += "\n";
-                content += mip.height;
-                content += "\n";
 
             }
+        }
 
-            var topmip = mips[0];
-            TextPrev.Content = content;
-            byte[] imagebytes = new byte[topmip.uncompressedSize];
-            Buffer.BlockCopy(exportEntry.Data, topmip.offset - exportEntry.DataOffset, imagebytes, 0, topmip.uncompressedSize);
-            AmaroK86.ImageFormat.DDS dds = new AmaroK86.ImageFormat.DDS(null, new AmaroK86.ImageFormat.ImageSize((uint)topmip.width, (uint)topmip.height), format.Value.Name.Substring(3), imagebytes);
+        private void LoadMip(Texture2DMipInfo mipToLoad)
+        {
+            if (mipToLoad == null ||  mipToLoad.storageType == StorageTypes.extUnc || mipToLoad.storageType == StorageTypes.empty) { return; }
+            byte[] imagebytes = new byte[mipToLoad.uncompressedSize];
+
+            if (mipToLoad.storageType == StorageTypes.pccUnc)
+            {
+                Buffer.BlockCopy(CurrentLoadedExport.Data, mipToLoad.offset - CurrentLoadedExport.DataOffset, imagebytes, 0, mipToLoad.uncompressedSize);
+            }
+            if ((mipToLoad.storageType == StorageTypes.extLZO || mipToLoad.storageType == StorageTypes.extZlib) && CurrentLoadedCacheName != null)
+            {
+                string filename = CurrentLoadedCacheName + ".tfc";
+                string localPathText = System.IO.Path.Combine(Directory.GetParent(CurrentLoadedExport.FileRef.FileName).FullName, filename);
+                if (File.Exists(localPathText))
+                {
+                    using (var file = File.OpenRead(localPathText))
+                    {
+                        int bytesRead;
+                        var buffer = new byte[mipToLoad.compressedSize];
+                        file.Seek(mipToLoad.offset, SeekOrigin.Begin);
+                        bytesRead = file.Read(buffer, 0, buffer.Length);
+
+                        if (mipToLoad.storageType == StorageTypes.extLZO){
+                            LZO2Helper.LZO2.Decompress(buffer, (uint)buffer.Length, imagebytes);
+                        }
+                        if (mipToLoad.storageType == StorageTypes.extZlib)
+                        {
+                            imagebytes = AmaroK86.MassEffect3.ZlibBlock.ZBlock.Decompress(file, mipToLoad.compressedSize);
+
+                            //new ZlibHelper.Zlib().Decompress(buffer, (uint)buffer.Length, imagebytes);
+                        }
+                    }
+                }
+            }
+            AmaroK86.ImageFormat.DDS dds = new AmaroK86.ImageFormat.DDS(null, new AmaroK86.ImageFormat.ImageSize((uint)mipToLoad.width, (uint)mipToLoad.height), CurrentLoadedFormat.Substring(3), imagebytes);
             AmaroK86.ImageFormat.DDSImage ddsimage = new AmaroK86.ImageFormat.DDSImage(dds.ToArray());
             var bitmap = ddsimage.mipMaps[0].bitmap;
             using (MemoryStream memory = new MemoryStream())
@@ -109,17 +146,50 @@ namespace ME3Explorer
 
         public override void UnloadExport()
         {
-            //throw new NotImplementedException();
+            TextureImage.Source = null;
+            CurrentLoadedFormat = null;
+            MipList.ClearEx();
         }
 
-        class Texture2DMipInfo
+        private void MipList_SelectedItemChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (MipList.Count > 0)
+            {
+                LoadMip(MipList[MipListView.SelectedIndex]);
+            }
+        }
+
+        public class Texture2DMipInfo
+        {
+            public int index;
             public int uncompressedSize;
             public int compressedSize;
             public int width;
             public int height;
             public int offset;
             public StorageTypes storageType;
+
+            public string MipDisplayString
+            {
+                get
+                {
+                    string mipinfostring = "Mip " + index;
+                    mipinfostring += "\nStorage Type: ";
+                    mipinfostring += storageType;
+                    mipinfostring += "\nUncompressed size: ";
+                    mipinfostring += uncompressedSize;
+                    mipinfostring += "\nCompressed size: ";
+                    mipinfostring += compressedSize;
+                    mipinfostring += "\nOffset: ";
+                    mipinfostring += offset;
+                    mipinfostring += "\nWidth: ";
+                    mipinfostring += width;
+                    mipinfostring += "\nHeight: ";
+                    mipinfostring += height;
+                    return mipinfostring;
+                }
+            }
         }
+
     }
 }
