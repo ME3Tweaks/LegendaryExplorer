748bbc70516bff97908d5c394a6650ea74d2f974
Kinkojiro <Kinkojiro@mail.com>
2019-04-12 16:18:02 +0100

    TLK Editor 1st edits
    Error.

---
+++ ME3Explorer/ME1TlkEditor/ME1TlkEditorWPF.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ME3Explorer.Packages;
+
+namespace ME3Explorer.ME1TlkEditor
+{
+    /// <summary>
+    /// Interaction logic for ME1TlkEditorWPF.xaml
+    /// </summary>
+    public partial class ME1TlkEditorWPF : ExportLoaderControl
+    {
+        public ME1TlkEditorWPF()
+        {
+            InitializeComponent();
+        }
+
+        public override bool CanParse(IExportEntry exportEntry)
+        {
+            return exportEntry.FileRef.Game == MEGame.ME1 && exportEntry.ClassName == "BioTlkFile";
+        }
+
+        public override void Dispose()
+        {
+
+        }
+
+        public override void LoadExport(IExportEntry exportEntry)
+        {
+
+        }
+
+        public override void UnloadExport()
+        {
+
+        }
+    }
+}
