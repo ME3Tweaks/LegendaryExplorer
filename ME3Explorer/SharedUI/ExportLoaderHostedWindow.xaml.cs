979d357a5a7659163e3e2633ed172b0e1659ba40
Mgamerz <mgamerz2@gmail.com>
2019-04-21 11:05:48 -0600

    Add generic Export Loader Hosted Window for popouts of export loaders

---
+++ ME3Explorer/SharedUI/ExportLoaderHostedWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using ME3Explorer.Packages;
+
+namespace ME3Explorer.SharedUI
+{
+    /// <summary>
+    /// Interaction logic for ExportLoaderHostedWindow.xaml
+    /// </summary>
+    public partial class ExportLoaderHostedWindow : Window
+    {
+        private IExportEntry exportToLoad;
+        private ExportLoaderControl hostedControl;
+        public ExportLoaderHostedWindow(ExportLoaderControl hostedControl, IExportEntry exportToLoad)
+        {
+            this.hostedControl = hostedControl;
+            this.exportToLoad = exportToLoad;
+            InitializeComponent();
+            RootPanel.Children.Add(hostedControl);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() =>
+            {
+                hostedControl.LoadExport(exportToLoad);
+                exportToLoad = null; //no reference
+            }));
+
+
+        }
+    }
+}
