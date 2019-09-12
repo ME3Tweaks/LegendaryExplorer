46a8ec2f25eed8f27dcbbb34fdb43d93ffc360e5
Kinkojiro <Kinkojiro@mail.com>
2019-09-12 20:31:00 +0100

    More groundwork
    Add xml helper parsing
    Switch for different games.

--- ME3Explorer/PropertyDatabase/PropertyDB.xaml.cs
+++ ME3Explorer/PropertyDatabase/PropertyDB.xaml.cs
@@ -3,6 +3,7 @@ using ME3Explorer.SharedUI;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,24 +16,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace ME3Explorer.PropertyDatabase
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for PropertyDB
     /// </summary>
     public partial class PropertyDB : WPFBase
     {
         #region Declarations
 
-        private MEGame currentGameDB;
+        public MEGame currentGame { get; set; }
 
-        
+        public PropsDataBase CurrentDataBase { get; set; }
+        private string CurrentDBPath { get; set; }
+
         public ICommand GenerateDBCommand { get; set; }
         public ICommand SaveDBCommand { get; set; }
-        public ICommand switchME1Command { get; set; }
-        public ICommand switchME2Command { get; set; }
-        public ICommand switchME3Command { get; set; }
+        public ICommand SwitchMECommand { get; set; }
+
+
+        public override void handleUpdate(List<PackageUpdate> updates)
+        {
+            throw new NotImplementedException();
+        }
 
         #endregion
 
@@ -44,19 +53,30 @@ namespace ME3Explorer.PropertyDatabase
             LoadCommands();
 
             //Get default path
-            //Get default / last game - set currentGameDB
-            //Load database
-            
+            CurrentDBPath = Properties.Settings.Default.PropertyDBPath;
+            Enum.TryParse<MEGame>(Properties.Settings.Default.PropertyDBGame, out MEGame game);
+
             InitializeComponent();
+
+            if (CurrentDBPath != null && CurrentDBPath.EndsWith("xml") && File.Exists(CurrentDBPath) && game != MEGame.Unknown)
+            {
+                SwitchGame(game.ToString());
+            }
+            else
+            {
+                CurrentDBPath = null;
+                SwitchGame("ME3");
+                StatusBar_LeftMostText.Text = "No database found.";
+            }
+
+            
         }
 
         private void LoadCommands()
         {
             GenerateDBCommand = new GenericCommand(GenerateDatabase);
             //SaveDBCommand = new GenericCommand();
-            switchME1Command = new GenericCommand(SwitchGameME1);
-            switchME2Command = new GenericCommand(SwitchGameME2);
-            switchME3Command = new GenericCommand(SwitchGameME3);
+            SwitchMECommand = new RelayCommand(SwitchGame);
         }
 
         private void PropertyDB_Closing(object sender, CancelEventArgs e)
@@ -65,43 +85,210 @@ namespace ME3Explorer.PropertyDatabase
                 return;
             //Dump Database
             //Save settings (path, currentGame)
+            Properties.Settings.Default.PropertyDBPath = CurrentDBPath;
+            Properties.Settings.Default.PropertyDBGame = currentGame.ToString();
+        }
+
+        #endregion
+
+        #region UserCommands
+        public void GenerateDatabase()
+        {
+            //Use Property Dumper - run directly? to generate?
+            //Save to XML
+        }
+
+        public void LoadDatabase()
+        {
+            if(CurrentDBPath == null)
+            {
+                //Open XML file
+                return;
+            }
+            //Select DB from Game
+            var filename = $"{System.IO.Path.GetDirectoryName(CurrentDBPath)}propertyDB{currentGame}.xml";
+            //Load database
+            CurrentDataBase = XmlHelper.FromXmlFile<PropsDataBase>(filename);
+
+
+        }
+
+        public void SaveDatabase()
+        {
+            //Set filename
+            //Save database to XML
+            var result = XmlHelper.ToXml(CurrentDataBase);
+        }
+
+
+        public void SwitchGame(object param)
+        {
+            var p = param as string;
+            switchME1_menu.IsChecked = false;
+            switchME2_menu.IsChecked = false;
+            switchME3_menu.IsChecked = false;
+            switch (p)
+            {
+                case "ME1":
+                    currentGame = MEGame.ME1;
+                    StatusBar_GameID_Text.Text = "ME1";
+                    StatusBar_GameID_Text.Background = new SolidColorBrush(Colors.Navy);
+                    switchME1_menu.IsChecked = true;
+                    break;
+                case "ME2":
+                    currentGame = MEGame.ME2;
+                    StatusBar_GameID_Text.Text = "ME2";
+                    StatusBar_GameID_Text.Background = new SolidColorBrush(Colors.Maroon);
+                    switchME2_menu.IsChecked = true;
+                    break;
+                default:
+                    currentGame = MEGame.ME3;
+                    StatusBar_GameID_Text.Text = "ME3";
+                    StatusBar_GameID_Text.Background = new SolidColorBrush(Colors.DarkSeaGreen);
+                    switchME3_menu.IsChecked = true;
+                    break;
+            }
+            
+            
+            LoadDatabase();
         }
 
         #endregion
 
 
-        public void GenerateDatabase()
+    }
+    #region Database
+    /// <summary>
+    /// Database Classes
+    /// </summary>
+    /// 
+    public class PropsDataBase
+    {
+        public MEGame meGame { get; set; }
+        public string GenerationDate { get; set; }
+        public List<ClassRecord> ClassRecords { get; set; }
+
+
+    }
+
+    public class ClassRecord
+    {
+        public string Class { get; set; }
+        public string Definition_package { get; set; }
+        public List<PropertyRecord> PropertyRecords { get; set; }
+
+    }
+
+    public class PropertyRecord
+    {
+        public string Property { get; set; }
+        public List<PropertyUsage> PropertyUsages { get; set; }
+
+    }
+
+    public class PropertyUsage
+    {
+        public string Filename { get; set; }
+        public string ExportUID { get; set; }
+        public bool IsDefault { get; set; }
+        public string Value { get; set; }
+
+    }
+    #endregion
+
+    #region XMLGen
+    public static class XmlHelper
+    {
+        public static bool NewLineOnAttributes { get; set; }
+        /// <summary>
+        /// Serializes an object to an XML string, using the specified namespaces.
+        /// </summary>
+        public static string ToXml(object obj, XmlSerializerNamespaces ns)
         {
-            //Use Property Dumper - run directly? to generate?
+            Type T = obj.GetType();
+
+            var xs = new XmlSerializer(T);
+            var ws = new XmlWriterSettings { Indent = true, NewLineOnAttributes = NewLineOnAttributes, OmitXmlDeclaration = true };
+
+            var sb = new StringBuilder();
+            using (XmlWriter writer = XmlWriter.Create(sb, ws))
+            {
+                xs.Serialize(writer, obj, ns);
+            }
+            return sb.ToString();
         }
 
-        public void LoadDatabase()
+        /// <summary>
+        /// Serializes an object to an XML string.
+        /// </summary>
+        public static string ToXml(object obj)
         {
-            //Dump existing database
-            //Load database
+            var ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+            return ToXml(obj, ns);
         }
 
-        public void SwitchGameME1()
+        /// <summary>
+        /// Deserializes an object from an XML string.
+        /// </summary>
+        public static PropsDataBase FromXml<T>(string xml)
         {
-            currentGameDB = MEGame.ME1;
-            LoadDatabase();
+            XmlSerializer xs = new XmlSerializer(typeof(PropsDataBase));
+            using (StringReader sr = new StringReader(xml))
+            {
+                return (PropsDataBase)xs.Deserialize(sr);
+            }
         }
 
-        public void SwitchGameME2()
+        /// <summary>
+        /// Deserializes an object from an XML string, using the specified type name.
+        /// </summary>
+        public static object FromXml(string xml, string typeName)
         {
-            currentGameDB = MEGame.ME2;
-            LoadDatabase();
+            Type T = Type.GetType(typeName);
+            XmlSerializer xs = new XmlSerializer(T);
+            using (StringReader sr = new StringReader(xml))
+            {
+                return xs.Deserialize(sr);
+            }
         }
 
-        public void SwitchGameME3()
+        /// <summary>
+        /// Serializes an object to an XML file.
+        /// </summary>
+        public static void ToXmlFile(Object obj, string filePath)
         {
-            currentGameDB = MEGame.ME3;
-            LoadDatabase();
+            var xs = new XmlSerializer(obj.GetType());
+            var ns = new XmlSerializerNamespaces();
+            var ws = new XmlWriterSettings { Indent = true, NewLineOnAttributes = NewLineOnAttributes, OmitXmlDeclaration = true };
+            ns.Add("", "");
+
+            using (XmlWriter writer = XmlWriter.Create(filePath, ws))
+            {
+                xs.Serialize(writer, obj);
+            }
         }
 
-        public override void handleUpdate(List<PackageUpdate> updates)
+        /// <summary>
+        /// Deserializes an object from an XML file.
+        /// </summary>
+        public static PropsDataBase FromXmlFile<T>(string filePath)
         {
-            throw new NotImplementedException();
+            StreamReader sr = new StreamReader(filePath);
+            try
+            {
+                var result = FromXml<PropsDataBase>(sr.ReadToEnd());
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("There was an error attempting to read the file " + filePath + "\n\n" + e.InnerException.Message);
+            }
+            finally
+            {
+                sr.Close();
+            }
         }
     }
+    #endregion
 }
