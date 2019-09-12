7d557e8f8d24d9472d64ed70f57085cf07d64a16
Mgamerz <mgamerz2@gmail.com>
2019-09-11 21:19:55 -0600

    Add recents menu and colorcoding to file hex viewer wpf. Disable viewing 1x1 mips to avoid native toolset crash

--- ME3Explorer/FileHexViewer/FileHexViewerWPF.xaml.cs
+++ ME3Explorer/FileHexViewer/FileHexViewerWPF.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -19,6 +20,7 @@ using Gibbed.IO;
 using ME3Explorer.Packages;
 using ME3Explorer.SharedUI;
 using Microsoft.Win32;
+using Path = System.IO.Path;
 
 namespace ME3Explorer.FileHexViewer
 {
@@ -30,12 +32,17 @@ namespace ME3Explorer.FileHexViewer
         //DO NOT USE WPFBASE - THIS IS NOT AN EDITOR
         private IMEPackage pcc;
         private byte[] bytes;
-        public HexBox Interpreter_Hexbox { get; private set; }
+        private List<string> RFiles;
+        private readonly string FileHexViewerDataFolder = Path.Combine(App.AppDataFolder, @"FileHexViewerWPF\");
+        private const string RECENTFILES_FILE = "RECENTFILES";
 
+        public HexBox Interpreter_Hexbox { get; private set; }
         public FileHexViewerWPF()
         {
             DataContext = this;
             InitializeComponent();
+            LoadRecentList();
+            RefreshRecent();
         }
 
         private void GotoOffset_Click(object sender, RoutedEventArgs e)
@@ -54,118 +61,139 @@ namespace ME3Explorer.FileHexViewer
             OpenFileDialog d = new OpenFileDialog();
             if (d.ShowDialog() == true)
             {
-                string lowerFilename = System.IO.Path.GetExtension(d.FileName).ToLower();
-                if (d.FileName.EndsWith(".pcc") || d.FileName.EndsWith(".u") || d.FileName.EndsWith(".sfm") || d.FileName.EndsWith(".upk"))
+                LoadFile(d.FileName);
+            }
+        }
+
+        private void LoadFile(string fileName)
+        {
+            string lowerFilename = Path.GetExtension(fileName).ToLower();
+            if (lowerFilename.EndsWith(".pcc") || lowerFilename.EndsWith(".u") || lowerFilename.EndsWith(".sfm") || lowerFilename.EndsWith(".upk"))
+            {
+                pcc = MEPackageHandler.OpenMEPackage(fileName);
+            }
+
+            bytes = File.ReadAllBytes(fileName);
+            Interpreter_Hexbox.ByteProvider = new DynamicByteProvider(bytes);
+            Title = "FileHexViewerWPF - " + fileName;
+            AddRecent(fileName, false);
+            SaveRecentList();
+            RefreshRecent();
+            MemoryStream inStream = new MemoryStream(bytes);
+            UnusedSpaceList.ClearEx();
+            if (pcc != null)
+            {
+                List<UsedSpace> used = new List<UsedSpace>();
+                used.Add(new UsedSpace
                 {
-                    pcc = MEPackageHandler.OpenMEPackage(d.FileName);
+                    UsedFor = "Package Header",
+                    UsedSpaceStart = 0,
+                    UsedSpaceEnd = pcc.NameOffset
+                });
+
+                inStream.Seek(pcc.NameOffset, SeekOrigin.Begin);
+                for (int i = 0; i < pcc.NameCount; i++)
+                {
+                    int strLength = inStream.ReadValueS32();
+                    inStream.ReadString(strLength * -2, true, Encoding.Unicode);
                 }
 
-                bytes = File.ReadAllBytes(d.FileName);
-                Interpreter_Hexbox.ByteProvider = new DynamicByteProvider(bytes);
-
-                MemoryStream inStream = new MemoryStream(bytes);
-                UnusedSpaceList.ClearEx();
-                if (pcc != null)
+                used.Add(new UsedSpace
                 {
-                    List<UsedSpace> used = new List<UsedSpace>();
-                    used.Add(new UsedSpace
-                    {
-                        UsedFor = "Package Header",
-                        UsedSpaceStart = 0,
-                        UsedSpaceEnd = pcc.NameOffset
-                    });
+                    UsedFor = "Name Table",
+                    UsedSpaceStart = pcc.NameOffset,
+                    UsedSpaceEnd = (int)inStream.Position
+                });
 
-                    inStream.Seek(pcc.NameOffset, SeekOrigin.Begin);
-                    for (int i = 0; i < pcc.NameCount; i++)
-                    {
-                        int strLength = inStream.ReadValueS32();
-                        inStream.ReadString(strLength * -2, true, Encoding.Unicode);
-                    }
-
-                    used.Add(new UsedSpace
-                    {
-                        UsedFor = "Name Table",
-                        UsedSpaceStart = pcc.NameOffset,
-                        UsedSpaceEnd = (int)inStream.Position
-                    });
-
-                    for (int i = 0; i < pcc.ImportCount; i++)
-                    {
-                        inStream.Position += 28;
-                    }
-
-                    used.Add(new UsedSpace
-                    {
-                        UsedFor = "Import Table",
-                        UsedSpaceStart = pcc.ImportOffset,
-                        UsedSpaceEnd = (int)inStream.Position
-                    });
-
-                    inStream.Seek(pcc.ExportOffset, SeekOrigin.Begin);
-                    for (int i = 0; i < pcc.ExportCount; i++)
-                    {
-                        inStream.Position += pcc.Exports[i].Header.Length;
-                    }
-
-                    used.Add(new UsedSpace
-                    {
-                        UsedFor = "Export Metadata Table",
-                        UsedSpaceStart = pcc.ExportOffset,
-                        UsedSpaceEnd = (int)inStream.Position
-                    });
-
-                    used.Add(new UsedSpace
-                    {
-                        UsedFor = "Dependency Table (Unused)",
-                        UsedSpaceStart = ((MEPackage)pcc).DependencyTableOffset,
-                        UsedSpaceEnd = ((MEPackage)pcc).FullHeaderSize
-                    });
-
-                    List<UsedSpace> usedExportsSpaces = new List<UsedSpace>();
-                    inStream.Seek(pcc.ExportOffset, SeekOrigin.Begin);
-                    for (int i = 0; i < pcc.ExportCount; i++)
-                    {
-                        ExportEntry exp = pcc.Exports[i];
-                        usedExportsSpaces.Add(new UsedSpace
-                        {
-                            UsedFor = $"Export {exp.UIndex}",
-                            UsedSpaceStart = exp.DataOffset,
-                            UsedSpaceEnd = exp.DataOffset + exp.DataSize
-                        });
-                    }
-
-                    usedExportsSpaces = usedExportsSpaces.OrderBy(x => x.UsedSpaceStart).ToList();
-                    List<UsedSpace> continuousBlocks = new List<UsedSpace>();
-                    UsedSpace continuous = new UsedSpace
-                    {
-                        UsedFor = "Continuous Export Data",
-                        UsedSpaceStart = usedExportsSpaces[0].UsedSpaceStart,
-                        UsedSpaceEnd = usedExportsSpaces[0].UsedSpaceEnd
-                    };
-
-                    for (int i = 1; i < usedExportsSpaces.Count; i++)
-                    {
-                        UsedSpace u = usedExportsSpaces[i];
-                        if (continuous.UsedSpaceEnd == u.UsedSpaceStart)
-                        {
-                            continuous.UsedSpaceEnd = u.UsedSpaceEnd;
-                        }
-                        else
-                        {
-                            continuousBlocks.Add(continuous);
-                            continuous = new UsedSpace
-                            {
-                                UsedFor = "Continuous Export Data",
-                                UsedSpaceStart = u.UsedSpaceStart,
-                                UsedSpaceEnd = u.UsedSpaceEnd
-                            };
-                        }
-
-                    }
-                    continuousBlocks.Add(continuous);
-                    UnusedSpaceList.AddRange(used);
-                    UnusedSpaceList.AddRange(continuousBlocks);
+                for (int i = 0; i < pcc.ImportCount; i++)
+                {
+                    inStream.Position += 28;
                 }
+
+                used.Add(new UsedSpace
+                {
+                    UsedFor = "Import Table",
+                    UsedSpaceStart = pcc.ImportOffset,
+                    UsedSpaceEnd = (int)inStream.Position
+                });
+
+                inStream.Seek(pcc.ExportOffset, SeekOrigin.Begin);
+                for (int i = 0; i < pcc.ExportCount; i++)
+                {
+                    inStream.Position += pcc.Exports[i].Header.Length;
+                }
+
+                used.Add(new UsedSpace
+                {
+                    UsedFor = "Export Metadata Table",
+                    UsedSpaceStart = pcc.ExportOffset,
+                    UsedSpaceEnd = (int)inStream.Position
+                });
+
+                used.Add(new UsedSpace
+                {
+                    UsedFor = "Dependency Table (Unused)",
+                    UsedSpaceStart = ((MEPackage)pcc).DependencyTableOffset,
+                    UsedSpaceEnd = ((MEPackage)pcc).FullHeaderSize
+                });
+
+                List<UsedSpace> usedExportsSpaces = new List<UsedSpace>();
+                inStream.Seek(pcc.ExportOffset, SeekOrigin.Begin);
+                for (int i = 0; i < pcc.ExportCount; i++)
+                {
+                    ExportEntry exp = pcc.Exports[i];
+                    usedExportsSpaces.Add(new UsedSpace
+                    {
+                        UsedFor = $"Export {exp.UIndex}",
+                        UsedSpaceStart = exp.DataOffset,
+                        UsedSpaceEnd = exp.DataOffset + exp.DataSize
+                    });
+                }
+
+                usedExportsSpaces = usedExportsSpaces.OrderBy(x => x.UsedSpaceStart).ToList();
+                List<UsedSpace> continuousBlocks = new List<UsedSpace>();
+                UsedSpace continuous = new UsedSpace
+                {
+                    UsedFor = "Continuous Export Data",
+                    UsedSpaceStart = usedExportsSpaces[0].UsedSpaceStart,
+                    UsedSpaceEnd = usedExportsSpaces[0].UsedSpaceEnd
+                };
+
+                for (int i = 1; i < usedExportsSpaces.Count; i++)
+                {
+                    UsedSpace u = usedExportsSpaces[i];
+                    if (continuous.UsedSpaceEnd == u.UsedSpaceStart)
+                    {
+                        continuous.UsedSpaceEnd = u.UsedSpaceEnd;
+                    }
+                    else
+                    {
+                        if (continuous.UsedSpaceEnd > u.UsedSpaceStart)
+                        {
+                            Debug.WriteLine("Possible overlap detected!");
+                        }
+                        continuousBlocks.Add(continuous);
+                        UsedSpace unused = new UsedSpace()
+                        {
+                            UsedFor = "Unused space",
+                            UsedSpaceStart = continuous.UsedSpaceEnd,
+                            UsedSpaceEnd = u.UsedSpaceStart,
+                            Unused = true
+                        };
+                        continuousBlocks.Add(unused);
+
+                        continuous = new UsedSpace
+                        {
+                            UsedFor = "Continuous Export Data",
+                            UsedSpaceStart = u.UsedSpaceStart,
+                            UsedSpaceEnd = u.UsedSpaceEnd
+                        };
+                    }
+
+                }
+                continuousBlocks.Add(continuous);
+                UnusedSpaceList.AddRange(used);
+                UnusedSpaceList.AddRange(continuousBlocks);
             }
         }
 
@@ -233,13 +261,113 @@ namespace ME3Explorer.FileHexViewer
             }
         }
 
+        #region Recents
+        private void LoadRecentList()
+        {
+            Recents_MenuItem.IsEnabled = false;
+            RFiles = new List<string>();
+            string path = FileHexViewerDataFolder + RECENTFILES_FILE;
+            if (File.Exists(path))
+            {
+                string[] recents = File.ReadAllLines(path);
+                foreach (string recent in recents)
+                {
+                    if (File.Exists(recent))
+                    {
+                        AddRecent(recent, true);
+                    }
+                }
+            }
+        }
+
+        private void SaveRecentList()
+        {
+            if (!Directory.Exists(FileHexViewerDataFolder))
+            {
+                Directory.CreateDirectory(FileHexViewerDataFolder);
+            }
+            string path = FileHexViewerDataFolder + RECENTFILES_FILE;
+            if (File.Exists(path))
+                File.Delete(path);
+            File.WriteAllLines(path, RFiles);
+        }
+
+        public void RefreshRecent()
+        {
+            Recents_MenuItem.Items.Clear();
+            if (RFiles.Count <= 0)
+            {
+                Recents_MenuItem.IsEnabled = false;
+                return;
+            }
+            Recents_MenuItem.IsEnabled = true;
+
+            int i = 0;
+            foreach (string filepath in RFiles)
+            {
+                MenuItem fr = new MenuItem()
+                {
+                    Header = filepath.Replace("_", "__"),
+                    Tag = filepath
+                };
+                fr.Click += RecentFile_click;
+                Recents_MenuItem.Items.Add(fr);
+                i++;
+            }
+        }
+
+        private void RecentFile_click(object sender, EventArgs e)
+        {
+            string s = ((FrameworkElement)sender).Tag.ToString();
+            if (File.Exists(s))
+            {
+                LoadFile(s);
+            }
+            else
+            {
+                MessageBox.Show("File does not exist: " + s);
+            }
+        }
+
+        public void AddRecent(string s, bool loadingList)
+        {
+            RFiles = RFiles.Where(x => !x.Equals(s, StringComparison.InvariantCultureIgnoreCase)).ToList();
+            if (loadingList)
+            {
+                RFiles.Add(s); //in order
+            }
+            else
+            {
+                RFiles.Insert(0, s); //put at front
+            }
+            if (RFiles.Count > 10)
+            {
+                RFiles.RemoveRange(10, RFiles.Count - 10);
+            }
+            Recents_MenuItem.IsEnabled = true;
+        }
+
+        #endregion
+
         public class UsedSpace
         {
             public int UsedSpaceStart { get; set; }
             public int UsedSpaceEnd { get; set; }
             public string UsedFor { get; set; }
+            public bool Unused { get; internal set; }
+            public long Length => UsedSpaceEnd - UsedSpaceStart;
 
             public override string ToString() => $"{UsedFor} 0x{UsedSpaceStart:X6} - 0x{UsedSpaceEnd:X6}";
+        }
+
+        private void FileHexViewer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                var space = ((UsedSpace) e.AddedItems[0]);
+                Interpreter_Hexbox.SelectionStart = space.UsedSpaceStart;
+                Interpreter_Hexbox.SelectionLength = space.Length;
+            }
         }
     }
 }
