a672bf82f701e06f75f91e33abb1508220282cda
SirCxyrtyx <sircxyrtyx@gmail.com>
2019-11-13 20:53:42 -0800

    Add TextureCube to the binary relinking exclusion list

--- ME3Explorer/CloningImportingAndRelinking/Relinker.cs
+++ ME3Explorer/CloningImportingAndRelinking/Relinker.cs
@@ -216,6 +216,7 @@ namespace ME3Explorer
                         case "TextureMovie":
                         case "AnimSequence":
                         case "RB_BodySetup":
+                        case "TextureCube":
                         case "FaceFXAsset":
                         case "MorphTarget":
                         case "ShadowMap1D":
