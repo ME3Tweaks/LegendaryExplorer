598dae531cb2628e73c68b437668a4130ae00e32
Mgamerz <mgamerz2@gmail.com>
2020-04-16 22:30:42 -0600

    Package Editor WPF: Left align on tree view select, maybe

--- ME3Explorer/PackageEditor/PackageEditorWPF.xaml.cs
+++ ME3Explorer/PackageEditor/PackageEditorWPF.xaml.cs
@@ -4975,5 +4975,36 @@ namespace ME3Explorer
                 });
             }
         }
+        private void TreeViewItem_RequestBringIntoView(object sender, RequestBringIntoViewEventArgs e)
+        {
+            // Ignore re-entrant calls
+            if (mSuppressRequestBringIntoView)
+                return;
+
+            // Cancel the current scroll attempt
+            e.Handled = true;
+
+            // Call BringIntoView using a rectangle that extends into "negative space" to the left of our
+            // actual control. This allows the vertical scrolling behaviour to operate without adversely
+            // affecting the current horizontal scroll position.
+            mSuppressRequestBringIntoView = true;
+
+            TreeViewItem tvi = sender as TreeViewItem;
+            if (tvi != null)
+            {
+                Rect newTargetRect = new Rect(-1000, 0, tvi.ActualWidth + 1000, tvi.ActualHeight);
+                tvi.BringIntoView(newTargetRect);
+            }
+
+            mSuppressRequestBringIntoView = false;
+        }
+        private bool mSuppressRequestBringIntoView;
+
+        // Correctly handle programmatically selected items
+        private void OnSelected(object sender, RoutedEventArgs e)
+        {
+            ((TreeViewItem)sender).BringIntoView();
+            e.Handled = true;
+        }
     }
 }
