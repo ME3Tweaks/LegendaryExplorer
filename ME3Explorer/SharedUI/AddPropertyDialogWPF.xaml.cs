ccdc14bbbfbef25ad69202646bf68fcd6743dca6
Mgamerz <mgamerz2@gmail.com>
2020-10-04 16:30:21 -0600

    Add Property dialog: Add filtering support

--- ME3Explorer/SharedUI/AddPropertyDialogWPF.xaml.cs
+++ ME3Explorer/SharedUI/AddPropertyDialogWPF.xaml.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
+using ME3ExplorerCore.Gammtek.Extensions;
+using ME3ExplorerCore.Helpers;
+using ME3ExplorerCore.Misc;
 using ME3ExplorerCore.Packages;
 using ME3ExplorerCore.Unreal;
 
@@ -13,28 +20,159 @@ namespace ME3Explorer.SharedUI
     /// </summary>
     public partial class AddPropertyDialogWPF : NotifyPropertyChangedWindowBase
     {
-        public List<string> existingProperties;
-        Dictionary<string, ClassInfo> classList;
-        
+        public class AddPropertyItem
+        {
+            public AddPropertyItem() { }
+
+            public AddPropertyItem(string propname, PropertyInfo propInfo)
+            {
+                this.PropertyName = propname;
+                this.PropInfo = propInfo;
+            }
+
+            public string PropertyName { get; }
+            public PropertyInfo PropInfo { get; }
+        }
+
+        public AddPropertyDialogWPF(List<ClassInfo> classList, List<string> _existingProperties)
+        {
+            _classHierarchy.ReplaceAll(classList.Select(x => x.ClassName));
+            existingProperties = _existingProperties;
+            classToClassPropertyMap = classList.ToDictionary(x => x.ClassName,
+                x => x.properties.Select(y => new AddPropertyItem(y.Key, y.Value)).ToList());
+            LoadCommands();
+            InitializeComponent();
+
+            ClassesView.Filter = FilterClass;
+            PropertiesView.Filter = FilterProperty;
+            SelectedClassName = _classHierarchy.Reverse().FirstOrDefault(x => classToClassPropertyMap[x].Any(FilterProperty));
+        }
+
+        private bool FilterClass(object obj)
+        {
+            if (obj is string className)
+            {
+                if (string.IsNullOrWhiteSpace(FilterText)) return true; //no filter
+                var props = classToClassPropertyMap[className];
+                return props.Any(FilterProperty);
+            }
+
+            return false;
+        }
+
+        private bool FilterProperty(object obj)
+        {
+            if (obj is AddPropertyItem api)
+            {
+                if (api.PropInfo.Transient) return false; //Don't show transient props
+                if (existingProperties.Contains(api.PropertyName, StringComparer.InvariantCultureIgnoreCase)) return false; //Don't show existing properties
+                if (string.IsNullOrWhiteSpace(FilterText)) return true; //no filter
+                if (api.PropertyName.Contains(FilterText, StringComparison.InvariantCultureIgnoreCase)) 
+                    return true;
+                if (api.PropInfo.Type.ToString().Contains(FilterText, StringComparison.InvariantCultureIgnoreCase)) 
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Properties already attached to our export (that should not be shown)
+        /// </summary>
+        private List<string> existingProperties;
+        /// <summary>
+        /// Mapping of class names to the class properties
+        /// </summary>
+
+        private Dictionary<string, List<AddPropertyItem>> classToClassPropertyMap;
+
+
+        #region Binding properties
+        // The selected class
         private string _selectedClassName;
         public string SelectedClassName
         {
             get => _selectedClassName;
-            set => SetProperty(ref _selectedClassName, value);
+            set
+            {
+                if (SetProperty(ref _selectedClassName, value) && value != null)
+                {
+                    updateShownProperties();
+                }
+            }
         }
 
-        public AddPropertyDialogWPF()
+        // The selected property object
+        private AddPropertyItem _selectedProperty;
+        public AddPropertyItem SelectedProperty
         {
-            LoadCommands();
-            InitializeComponent();
+            get => _selectedProperty;
+            set => SetProperty(ref _selectedProperty, value);
         }
 
+        // The current filter text
+        private string _filterText;
+        public string FilterText
+        {
+            get => _filterText;
+            set
+            {
+                if (SetProperty(ref _filterText, value))
+                {
+                    // May need to reselect?
+                    var selectedClassName = SelectedClassName;
+                    ClassesView.Refresh();
+                    PropertiesView.Refresh();
+                    if (!ClassesView.Contains(selectedClassName))
+                    {
+                        // Done this way since there's no index accessors
+                        foreach (var v in ClassesView)
+                        {
+                            if (v is string str)
+                            {
+                                SelectedClassName = str;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private ObservableCollectionExtended<string> _classHierarchy { get; } = new ObservableCollectionExtended<string>();
+        public ICollectionView ClassesView => CollectionViewSource.GetDefaultView(_classHierarchy);
+        private ObservableCollectionExtended<AddPropertyItem> _availableProperties { get; } = new ObservableCollectionExtended<AddPropertyItem>();
+        public ICollectionView PropertiesView => CollectionViewSource.GetDefaultView(_availableProperties);
+
+        #endregion
+
+        private void updateShownProperties()
+        {
+            // Replaces the list of available properties with ones for the specified selected class
+            // that are not transient and are not already part of the export
+            using (PropertiesView.DeferRefresh())
+            {
+                if (SelectedClassName != null)
+                {
+                    _availableProperties.ReplaceAll(classToClassPropertyMap[SelectedClassName]
+                        .Where(x => !x.PropInfo.Transient && !existingProperties.Contains(x.PropertyName))
+                        .OrderBy(x => x.PropertyName));
+                }
+                else
+                {
+                    _availableProperties.ClearEx();
+                }
+            }
+        }
+
+        #region Commands
         public ICommand AddPropertyCommand { get; set; }
 
         private void LoadCommands()
         {
             AddPropertyCommand = new GenericCommand(AddProperty, CanAddProperty);
         }
+
 
         private void AddProperty()
         {
@@ -44,14 +182,17 @@ namespace ME3Explorer.SharedUI
 
         private bool CanAddProperty()
         {
-            return PropertiesListView.SelectedItem != null;
+            return SelectedProperty != null;
         }
+
+        #endregion
+
+
 
         public static (string, PropertyInfo)? GetProperty(ExportEntry export, List<string> _existingProperties, MEGame game, Window callingWindow = null)
         {
-            string origname = export.ClassName;
             string temp = export.ClassName;
-            var classes = new List<string>();
+            var classes = new List<ClassInfo>();
             Dictionary<string, ClassInfo> classList;
             switch (game)
             {
@@ -66,15 +207,7 @@ namespace ME3Explorer.SharedUI
                     classList = ME3UnrealObjectInfo.Classes;
                     break;
             }
-            //For debugging ME1 Objectinfo when we get around to it
-            //foreach (KeyValuePair<string, ClassInfo> entry in classList)
-            //{
-            //    // do something with entry.Value or entry.Key
-            //    if (entry.Key.StartsWith("LightMap"))
-            //    {
-            //        Debug.WriteLine(entry.Key);
-            //    }
-            //}
+
             if (!classList.ContainsKey(temp) && export.Class is ImportEntry)
             {
                 //lookup import parent info
@@ -115,42 +248,29 @@ namespace ME3Explorer.SharedUI
             }
             while (classList.ContainsKey(temp) && temp != "Object")
             {
-                classes.Add(temp);
+                classes.Add(classList[temp]);
                 temp = classList[temp].baseClass;
             }
             classes.Reverse();
-            AddPropertyDialogWPF prompt = new AddPropertyDialogWPF();
-            if (callingWindow != null)
+            AddPropertyDialogWPF prompt = new AddPropertyDialogWPF(classes, _existingProperties)
             {
-                prompt.Owner = callingWindow;
-            }
-            prompt.classList = classList;
-            prompt.existingProperties = _existingProperties;
-            prompt.ClassesListView.ItemsSource = classes;
-            prompt.ClassesListView.SelectedItem = origname;
+                Owner = callingWindow
+            };
+            //prompt.ClassesListView.ItemsSource = classes;
+            //prompt.ClassesListView.SelectedItem = origname;
             prompt.ShowDialog();
             if (prompt.DialogResult.HasValue
              && prompt.DialogResult.Value
-             && prompt.PropertiesListView.SelectedIndex != -1
-             && prompt.PropertiesListView.SelectedItem is KeyValuePair<string, PropertyInfo> kvp)
+             && prompt.SelectedProperty != null)
             {
-                return (kvp.Key, kvp.Value);
+                return (prompt.SelectedProperty.PropertyName, prompt.SelectedProperty.PropInfo);
             }
             return null;
         }
 
-        private void ClassesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            string className = (string)ClassesListView.SelectedItem;
-            SelectedClassName = className;
-            var props = classList[className].properties.Where(x => !x.Value.Transient && !existingProperties.Contains(x.Key)).OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
-            //props.Sort();
-            PropertiesListView.ItemsSource = props;
-        }
-
         private void PropertiesListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if (PropertiesListView.SelectedIndex >= 0)
+            if (SelectedProperty != null)
             {
                 DialogResult = true;
                 Close();
