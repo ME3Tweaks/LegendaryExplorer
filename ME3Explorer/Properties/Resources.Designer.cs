8ec307390bfa7e4ad4140f43cd9c273c73c66c0c
Mgamerz <mgamerz2@gmail.com>
2020-10-09 19:34:41 -0600

    Add missing source icon TLKEditor. Fix TLK Editor icon. Remove a bunch of unused assets

--- ME3Explorer/Properties/Resources.Designer.cs
+++ ME3Explorer/Properties/Resources.Designer.cs
@@ -61,16 +61,6 @@ namespace ME3Explorer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        public static System.Drawing.Bitmap interpreter_icon_64x64 {
-            get {
-                object obj = ResourceManager.GetObject("interpreter_icon_64x64", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         public static byte[] KismetFont {
