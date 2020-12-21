fcca36b7eca7cc54ac90cfc0b1f671ccf77466be
Mgamerz <mgamerz2@gmail.com>
2020-12-21 14:03:18 -0700

    Begin work on texture studio

---
+++ ME3Explorer/TextureStudio/TextureStudioUI.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using ME3ExplorerCore.Compression;
+using ME3ExplorerCore.Helpers;
+using ME3ExplorerCore.Packages;
+
+namespace ME3Explorer.TextureStudio
+{
+    /// <summary>
+    /// Interaction logic for TextureStudioUI.xaml
+    /// </summary>
+    public partial class TextureStudioUI : Window
+    {
+        public TextureStudioUI()
+        {
+            InitializeComponent();
+        }
+
+        private void LTM(object sender, RoutedEventArgs e)
+        {
+            MEMTextureMap.LoadTextureMap(MEGame.ME3);
+
+        }
+    }
+}
