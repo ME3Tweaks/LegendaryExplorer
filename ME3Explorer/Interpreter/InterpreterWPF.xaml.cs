bee0eafa54315ae17f78edb9d71779ab8fac5d5f
Mgamerz <mgamerz2@gmail.com>
2021-01-22 10:07:33 -0700

    InterpreterWPF: Support Static Array Index

--- ME3Explorer/Interpreter/InterpreterWPF.xaml.cs
+++ ME3Explorer/Interpreter/InterpreterWPF.xaml.cs
@@ -883,9 +883,15 @@ namespace ME3Explorer
         public static UPropertyTreeViewEntry GenerateUPropertyTreeViewEntry(Property prop, UPropertyTreeViewEntry parent, ExportEntry parsingExport, string displayPrefix = "", PropertyChangedEventHandler PropertyChangedHandler = null)
         {
             string displayName = displayPrefix;
+
             if (!(parent.Property is ArrayPropertyBase))
             {
-                displayName += $" {prop.Name.Instanced}:";
+                displayName += $" {prop.Name.Instanced}";
+                if (prop.StaticArrayIndex > 0)
+                {
+                    displayName += $"[{prop.StaticArrayIndex}]";
+                }
+                displayName += ":";
             }
             string editableValue = ""; //editable value
             string parsedValue = ""; //human formatted item. Will most times be blank
