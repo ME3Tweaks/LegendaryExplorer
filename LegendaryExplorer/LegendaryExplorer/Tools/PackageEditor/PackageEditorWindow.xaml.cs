0d6117d39c8bccd4cbfa8e648bba94e3c2c03c12
Mgamerz <mgamerz2@gmail.com>
2021-07-03 19:38:41 -0600

    Enable drag/drop from UDK to LE

--- LegendaryExplorer/LegendaryExplorer/Tools/PackageEditor/PackageEditorWindow.xaml.cs
+++ LegendaryExplorer/LegendaryExplorer/Tools/PackageEditor/PackageEditorWindow.xaml.cs
@@ -3201,6 +3201,7 @@ namespace LegendaryExplorer.Tools.PackageEditor
                 sourceItem.Parent != null)
             {
                 if (targetItem.Entry != null && sourceItem.Entry != null &&
+                    sourceItem.Entry.Game != MEGame.UDK && // allow UDK -> OT and LE
                     targetItem.Game.IsLEGame() != sourceItem.Entry.Game.IsLEGame())
                 {
                     MessageBox.Show(
