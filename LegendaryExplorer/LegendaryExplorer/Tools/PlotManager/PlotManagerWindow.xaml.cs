fedc9f3506a26417b35a2c788780779c15456640
Kinkojiro <Kinkojiro@mail.com>
2021-07-18 08:39:41 +0100

    Update Plot Database
    Add list view of selected plot states and children.
    Need to improve list creation (max 100 as temporary workaround).

--- LegendaryExplorer/LegendaryExplorer/Tools/PlotManager/PlotManagerWindow.xaml.cs
+++ LegendaryExplorer/LegendaryExplorer/Tools/PlotManager/PlotManagerWindow.xaml.cs
@@ -40,9 +40,19 @@ namespace LegendaryExplorer.Tools.PlotManager
         public PlotElement SelectedNode
         {
             get => _selectedNode;
-            set => SetProperty(ref _selectedNode, value);
+            set
+            {
+                if (SetProperty(ref _selectedNode, value))
+                {
+                    UpdateSelection();
+                }
+            }
         }
+        private int previousView { get; set; }
+        private int _currentView;
+        public int currentView { get => _currentView; set { previousView = _currentView; SetProperty(ref _currentView, value); } }
 
+        private int tempTreeCount; //Temporary stop large table generation.
         public PlotManagerWindow()
         {
 
@@ -51,12 +61,14 @@ namespace LegendaryExplorer.Tools.PlotManager
 
             var dictionary3 = PlotDatabases.GetMasterDictionaryForGame(MEGame.LE3);
             RootNodes3.Add(dictionary3[1]);
-            
+
             var dictionary2 = PlotDatabases.GetMasterDictionaryForGame(MEGame.LE2);
             RootNodes2.Add(dictionary2[1]);
             var dictionary1 = PlotDatabases.GetMasterDictionaryForGame(MEGame.LE1);
             RootNodes1.Add(dictionary1[1]);
             Focus();
+           
+            
         }
 
 
@@ -65,6 +77,55 @@ namespace LegendaryExplorer.Tools.PlotManager
 
         }
 
+
+        private void UpdateSelection()
+        {
+            tempTreeCount = 0;
+            Elements3.ClearEx();
+            tempTreeCount++;
+            AddPlotsToList(SelectedNode, Elements3);
+        }
+
+        private void AddPlotsToList(PlotElement plotElement, ObservableCollectionExtended<PlotElement> elementList)
+        {
+            switch (plotElement.Type)
+            {
+                case PlotElementType.Plot:
+                case PlotElementType.Region:
+                case PlotElementType.FlagGroup:
+                case PlotElementType.None:
+                    break;
+                default:
+                    elementList.Add(plotElement);
+                    tempTreeCount++;
+                    break;
+            }
+
+            foreach (var c in plotElement.Children)
+            {
+                if(tempTreeCount < 100)
+                {
+                    AddPlotsToList(c, elementList);
+                }
+                
+            }
+        }
+
+        private void Tree_BW3_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            SelectedNode = Tree_BW3.SelectedItem as PlotElement;
+            
+        }
+
+        private void Tree_BW2_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            SelectedNode = Tree_BW2.SelectedItem as PlotElement;
+        }
+
+        private void Tree_BW1_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            SelectedNode = Tree_BW1.SelectedItem as PlotElement;
+        }
     }
 
   
