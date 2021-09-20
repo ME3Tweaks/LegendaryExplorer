a7578c7972fb76366f904f0edc83f84063a88f4a
Exkywor <fernandoquant.dev@gmail.com>
2021-09-20 10:35:30 -0600

    Add an Experiments menu to the InterpEditor. Add Exkywor's experiments for the InterpEditor

---
+++ LegendaryExplorer/LegendaryExplorer/Tools/InterpEditor/InterpExperiments/InterpEditorExperimentsE.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Threading;
+using LegendaryExplorer.Dialogs;
+using LegendaryExplorer.Misc;
+using LegendaryExplorer.Tools.PackageEditor;
+using LegendaryExplorerCore.Helpers;
+using LegendaryExplorerCore.GameFilesystem;
+using LegendaryExplorerCore.Matinee;
+using LegendaryExplorerCore.Misc;
+using LegendaryExplorerCore.Packages;
+using LegendaryExplorerCore.TLK.ME1;
+using LegendaryExplorerCore.TLK.ME2ME3;
+using LegendaryExplorerCore.Unreal;
+using LegendaryExplorerCore.Unreal.BinaryConverters;
+using LegendaryExplorerCore.Unreal.ObjectInfo;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Newtonsoft.Json;
+using HuffmanCompression = LegendaryExplorerCore.TLK.ME1.HuffmanCompression;
+
+namespace LegendaryExplorer.Tools.InterpEditor.InterpExperiments
+{
+    /// <summary>
+    /// Class for Exkywor's preset buttons and stuff
+    /// </summary>
+    class InterpEditorExperimentsE
+    {
+        public static void AddPresetGroup(string preset, InterpEditorWindow iew)
+        {
+            var currExp = iew.Properties_InterpreterWPF.CurrentLoadedExport;
+
+            if (currExp != null)
+            {
+
+                if (currExp.ClassName != "InterpData")
+                {
+                    MessageBox.Show("InterpData not selected.", "Warning", MessageBoxButton.OK);
+                    return;
+                }
+
+                switch (preset)
+                {
+                    case "Director":
+                        MatineeHelper.AddPresetDirectorGroup(currExp);
+                        break;
+
+                    case "Camera":
+                        if (PromptDialog.Prompt(null, "Name of camera actor:") is string camName)
+                        {
+                            if (string.IsNullOrEmpty(camName))
+                            {
+                                MessageBox.Show("Not a valid camera actor name.", "Warning", MessageBoxButton.OK);
+                                return;
+                            }
+                            MatineeHelper.AddPresetCameraGroup(currExp, camName);
+                        }
+                        break;
+                }
+            }
+            return;
+        }
+
+        public static void AddPresetTrack(string preset, InterpEditorWindow iew)
+        {
+            var currExp = iew.Properties_InterpreterWPF.CurrentLoadedExport;
+
+            if (currExp != null)
+            {
+                if (currExp.ClassName != "InterpGroup")
+                {
+                    MessageBox.Show("InterpGroup not selected.", "Warning", MessageBoxButton.OK);
+                    return;
+                }
+
+                switch (preset)
+                {
+                    case "Gesture":
+                        if (PromptDialog.Prompt(null, "Name of gesture actor:") is string actor)
+                        {
+                            if (string.IsNullOrEmpty(actor))
+                            {
+                                MessageBox.Show("Not a valid gesture actor name.", "Warning", MessageBoxButton.OK);
+                                return;
+                            }
+                            MatineeHelper.AddPresetGestureTrack(currExp, actor);
+                        }
+                        break;
+                }
+            }
+            return;
+        }
+    }
+}
---
+++ LegendaryExplorer/LegendaryExplorer/UserControls/InterpEditorControls/InterpExperimentsMenuControl.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using LegendaryExplorer.Dialogs;
+using LegendaryExplorer.Misc;
+using LegendaryExplorer.SharedUI;
+using LegendaryExplorer.Tools.InterpEditor;
+using LegendaryExplorer.Tools.InterpEditor.InterpExperiments;
+using LegendaryExplorer.Tools.PackageEditor;
+using LegendaryExplorer.UserControls.ExportLoaderControls;
+using LegendaryExplorerCore.GameFilesystem;
+using LegendaryExplorerCore.Helpers;
+using LegendaryExplorerCore.Misc;
+using LegendaryExplorerCore.Packages;
+using LegendaryExplorerCore.TLK.ME1;
+using LegendaryExplorerCore.Unreal;
+using LegendaryExplorerCore.Unreal.BinaryConverters;
+using LegendaryExplorerCore.Unreal.ObjectInfo;
+using LegendaryExplorerCore.UnrealScript;
+using LegendaryExplorerCore.UnrealScript.Compiling.Errors;
+using LegendaryExplorerCore.UnrealScript.Language.Tree;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Newtonsoft.Json;
+
+namespace LegendaryExplorer.UserControls.InterpEditorControls
+{
+    /// <summary>
+    /// Class that holds toolset development experiments. Actual experiment code should be in the Experiments classes
+    /// </summary>
+    public partial class InterpExperimentsMenuControl : MenuItem
+    {
+        public InterpExperimentsMenuControl()
+        {
+            LoadCommands();
+            InitializeComponent();
+        }
+
+        private void LoadCommands() { }
+
+        public InterpEditorWindow GetIEWindow()
+        {
+            if (Window.GetWindow(this) is InterpEditorWindow iew)
+            {
+                return iew;
+            }
+
+            return null;
+        }
+
+        // EXPERIMENTS: EXKYWOR------------------------------------------------------------
+        #region Exkywor's experiments
+        private void AddPresetDirectorGroup_Click(object sender, RoutedEventArgs e)
+        {
+            InterpEditorExperimentsE.AddPresetGroup("Director", GetIEWindow());
+        }
+
+        private void AddPresetCameraGroup_Click(object sender, RoutedEventArgs e)
+        {
+            InterpEditorExperimentsE.AddPresetGroup("Camera", GetIEWindow());
+        }
+
+        private void AddPresetGestureTrack_Click(object sender, RoutedEventArgs e)
+        {
+            InterpEditorExperimentsE.AddPresetTrack("Gesture", GetIEWindow());
+        }
+        #endregion
+    }
+}
