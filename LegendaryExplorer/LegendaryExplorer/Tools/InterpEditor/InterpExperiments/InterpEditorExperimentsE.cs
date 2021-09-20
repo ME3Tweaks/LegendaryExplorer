8678a584d18ce287faebaeda67be79ea6ae0f889
Exkywor <fernandoquant.dev@gmail.com>
2021-09-20 10:50:33 -0600

    Remove unnecessary imports from Exkywor's experiments

--- LegendaryExplorer/LegendaryExplorer/Tools/InterpEditor/InterpExperiments/InterpEditorExperimentsE.cs
+++ LegendaryExplorer/LegendaryExplorer/Tools/InterpEditor/InterpExperiments/InterpEditorExperimentsE.cs
@@ -1,30 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Numerics;
-using System.Runtime.InteropServices;
-using System.Windows;
-using System.Windows.Threading;
+﻿using System.Windows;
 using LegendaryExplorer.Dialogs;
-using LegendaryExplorer.Misc;
-using LegendaryExplorer.Tools.PackageEditor;
-using LegendaryExplorerCore.Helpers;
-using LegendaryExplorerCore.GameFilesystem;
 using LegendaryExplorerCore.Matinee;
-using LegendaryExplorerCore.Misc;
-using LegendaryExplorerCore.Packages;
-using LegendaryExplorerCore.TLK.ME1;
-using LegendaryExplorerCore.TLK.ME2ME3;
-using LegendaryExplorerCore.Unreal;
-using LegendaryExplorerCore.Unreal.BinaryConverters;
-using LegendaryExplorerCore.Unreal.ObjectInfo;
-using Microsoft.WindowsAPICodePack.Dialogs;
-using Newtonsoft.Json;
-using HuffmanCompression = LegendaryExplorerCore.TLK.ME1.HuffmanCompression;
 
 namespace LegendaryExplorer.Tools.InterpEditor.InterpExperiments
 {
--- LegendaryExplorer/LegendaryExplorer/UserControls/InterpEditorControls/InterpExperimentsMenuControl.xaml.cs
+++ LegendaryExplorer/LegendaryExplorer/UserControls/InterpEditorControls/InterpExperimentsMenuControl.xaml.cs
@@ -1,34 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Threading;
-using LegendaryExplorer.Dialogs;
-using LegendaryExplorer.Misc;
-using LegendaryExplorer.SharedUI;
 using LegendaryExplorer.Tools.InterpEditor;
 using LegendaryExplorer.Tools.InterpEditor.InterpExperiments;
-using LegendaryExplorer.Tools.PackageEditor;
-using LegendaryExplorer.UserControls.ExportLoaderControls;
-using LegendaryExplorerCore.GameFilesystem;
-using LegendaryExplorerCore.Helpers;
-using LegendaryExplorerCore.Misc;
-using LegendaryExplorerCore.Packages;
-using LegendaryExplorerCore.TLK.ME1;
-using LegendaryExplorerCore.Unreal;
-using LegendaryExplorerCore.Unreal.BinaryConverters;
-using LegendaryExplorerCore.Unreal.ObjectInfo;
-using LegendaryExplorerCore.UnrealScript;
-using LegendaryExplorerCore.UnrealScript.Compiling.Errors;
-using LegendaryExplorerCore.UnrealScript.Language.Tree;
-using Microsoft.Win32;
-using Microsoft.WindowsAPICodePack.Dialogs;
-using Newtonsoft.Json;
 
 namespace LegendaryExplorer.UserControls.InterpEditorControls
 {
