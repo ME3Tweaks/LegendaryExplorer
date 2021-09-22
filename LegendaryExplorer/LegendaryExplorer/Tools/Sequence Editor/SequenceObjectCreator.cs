25b20a3627251254f0c74e3edf60622bc99e72ad
Mgamerz <mgamerz2@gmail.com>
2021-09-21 23:46:31 -0600

    SequenceObjectCreator: Fix null reference exception when package cache is attempted to be used but none was passed in

--- LegendaryExplorer/LegendaryExplorer/Tools/Sequence Editor/SequenceObjectCreator.cs
+++ LegendaryExplorer/LegendaryExplorer/Tools/Sequence Editor/SequenceObjectCreator.cs
@@ -71,6 +71,7 @@ namespace LegendaryExplorer.Tools.Sequence_Editor
 
         public static PropertyCollection GetSequenceObjectDefaults(IMEPackage pcc, ClassInfo info)
         {
+            pc ??= new PackageCache();
             MEGame game = pcc.Game;
             PropertyCollection defaults = new();
             if (info.ClassName == "Sequence")
