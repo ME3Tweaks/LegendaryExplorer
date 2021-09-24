93b23f3498eb926c4bfd8fe0bbd038449b2c0b18
Mgamerz <mgamerz2@gmail.com>
2021-09-23 19:43:48 -0600

    Add BioInert object binary class. Not 100% it's accurate.

---
+++ LegendaryExplorer/LegendaryExplorerCore/Unreal/BinaryConverters/BioInert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LegendaryExplorerCore.Misc;
+using LegendaryExplorerCore.Packages;
+
+namespace LegendaryExplorerCore.Unreal.BinaryConverters
+{
+    public class BioInert : ObjectBinary
+    {
+        public OrderedMultiValueDictionary<NameReference, Guid> NameGuidMap;//? Speculative name
+        protected override void Serialize(SerializingContainer2 sc)
+        {
+            sc.Serialize(ref NameGuidMap, SCExt.Serialize, SCExt.Serialize);
+        }
+
+        public override List<(UIndex, string)> GetUIndexes(MEGame game)
+        {
+            return new List<(UIndex, string)>(); // None
+        }
+
+        public override List<(NameReference, string)> GetNames(MEGame game)
+        {
+            var names = new List<(NameReference, string)>();
+
+            names.AddRange(NameGuidMap.Select((kvp, i) => (kvp.Key, $"Name[{i}]")));
+
+            return names;
+        }
+    }
+}
