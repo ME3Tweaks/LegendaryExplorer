95d7b77fd895e7be8c595eb191346427138d989b
Mgamerz <mgamerz2@gmail.com>
2021-09-24 01:03:46 -0600

    Fix serialization of BioInert

--- LegendaryExplorer/LegendaryExplorerCore/Unreal/BinaryConverters/BioInert.cs
+++ LegendaryExplorer/LegendaryExplorerCore/Unreal/BinaryConverters/BioInert.cs
@@ -6,26 +6,56 @@ using LegendaryExplorerCore.Packages;
 
 namespace LegendaryExplorerCore.Unreal.BinaryConverters
 {
+    public class NameEntryGuidPair
+    {
+        public NameReference Name;
+        public UIndex Entry;
+        public Guid GUID;
+    }
+
     public class BioInert : ObjectBinary
     {
-        public OrderedMultiValueDictionary<NameReference, Guid> NameGuidMap;//? Speculative name
+        public NameEntryGuidPair[] NameEntryGuidPairs;//? Speculative name
         protected override void Serialize(SerializingContainer2 sc)
         {
-            sc.Serialize(ref NameGuidMap, SCExt.Serialize, SCExt.Serialize);
+            sc.Serialize(ref NameEntryGuidPairs, SCExt.Serialize);
         }
 
         public override List<(UIndex, string)> GetUIndexes(MEGame game)
         {
-            return new List<(UIndex, string)>(); // None
+            List<(UIndex, string)> indices = new List<(UIndex, string)>(NameEntryGuidPairs.Length);
+            if (game == MEGame.LE1)
+            {
+                for (int i = 0; i < NameEntryGuidPairs.Length; i++)
+                {
+                    indices.Add((NameEntryGuidPairs[i].Entry, $"NameEntryGuid[{i}]")); // how to handle nulls?
+                }
+            }
+            return indices;
         }
 
         public override List<(NameReference, string)> GetNames(MEGame game)
         {
             var names = new List<(NameReference, string)>();
-
-            names.AddRange(NameGuidMap.Select((kvp, i) => (kvp.Key, $"Name[{i}]")));
-
+            names.AddRange(NameEntryGuidPairs.Select((kvp, i) => (kvp.Name, $"Name[{i}]")));
             return names;
+        }
+    }
+
+    public static partial class SCExt
+    {
+        public static void Serialize(this SerializingContainer2 sc, ref NameEntryGuidPair negp)
+        {
+            if (sc.IsLoading)
+            {
+                negp = new NameEntryGuidPair();
+            }
+            sc.Serialize(ref negp.Name);
+            if (sc.Game == MEGame.LE1)
+            {
+                sc.Serialize(ref negp.Entry);
+            }
+            sc.Serialize(ref negp.GUID);
         }
     }
 }
