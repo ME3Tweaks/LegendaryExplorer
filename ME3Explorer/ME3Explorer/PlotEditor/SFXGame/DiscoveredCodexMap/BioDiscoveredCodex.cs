443b1016c82133de8643aebda9d756a7fbb7d1ae
Mgamerz <mgamerz2@gmail.com>
2021-12-07 20:14:05 -0700

    Nuke ME3Explorer from the repo

--- ME3Explorer/ME3Explorer/PlotEditor/SFXGame/DiscoveredCodexMap/BioDiscoveredCodex.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Gammtek.Conduit.MassEffect3.SFXGame.DiscoveredCodexMap
-{
-	public class BioDiscoveredCodex : BioVersionedNativeObject
-	{
-		public List<BioDiscoveredCodexPage> DiscoveredPages { get; set; }
-	}
-}
