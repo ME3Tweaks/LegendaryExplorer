33627a73f1944d5ac1ae79b7be71aa6fbacb95c8
Mgamerz <mgamerz2@gmail.com>
2022-03-09 19:52:10 -0700

    SYNC COMMIT: Add initial ExtensionTests.cs

---
+++ LegendaryExplorer/LegendaryExplorerCore.Tests/ExtensionTests.cs
@@ -0,0 +1,65 @@
+﻿using LegendaryExplorerCore.Packages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendaryExplorerCore.Tests
+{
+    [TestClass]
+    public class ExtensionTests
+    {
+        [TestMethod]
+        public void TestUnrealLocalizationTests()
+        {
+            // Test GetLocalization()
+            Dictionary<string, MELocalization> testData = new Dictionary<string, MELocalization>()
+            {
+                { "Startup_INT", MELocalization.INT },
+                { "Startup_FRA", MELocalization.FRA },
+                { "EntryMenu_RUS", MELocalization.RUS },
+                { "TestFile_GE", MELocalization.DEU },
+                { "PackageFile.pcc", MELocalization.None },
+                { "PackageFile_200.pcc", MELocalization.None },
+                { "AhernsCrabShack_FR_DEU.pcc", MELocalization.DEU },
+                { "AhernsCrabShack_FR_JP.pcc", MELocalization.JPN },
+                { "LocTest1_LOC_JPN.pcc", MELocalization.JPN },
+                { "LocTest2_LOC_HERP.pcc", MELocalization.None },
+                { "LocTest2_LOC_INT.pcc", MELocalization.INT },
+                { "AhernsCrabShack_FR_JP.pcc", MELocalization.JPN },
+                { "C:\\Users\\CrabMan_FR\\Package_ITA.pcc", MELocalization.ITA },
+                { "C:\\Users\\CrabMan_FR\\Package_LOC_RUS.pcc", MELocalization.RUS },
+            };
+
+            foreach (var v in testData)
+            {
+                Assert.AreEqual(v.Value, v.Key.GetUnrealLocalization(), @"GetUnrealLocalization() returned the wrong localization!");
+            }
+
+            // Test StripLocalization()
+            Dictionary<string, string> testStrings = new Dictionary<string, string>()
+            {
+                { "Startup_INT", "Startup" },
+                { "Startup_FRA", "Startup" },
+                { "Startup_Bonk.pcc", "Startup" },
+                { "Startup_Derp", "Startup" },
+                { "FR_DEU_JP_INT.pcc", "FR_DEU_JP.pcc" },
+                { "BioD_Level1_200_LOC_INT", "Startup" },
+                { "Startup_INT", "Startup" },
+                { "C:\\Users\\OhNo.pcc", "C:\\Users\\OhNo.pcc" },
+                { "C:\\Users\\OhNo_LOC_DEU.pcc", "C:\\Users\\OhNo.pcc" },
+                { "C:\\Users\\OhNo_FR.pcc", "C:\\Users\\OhNo.pcc" },
+                { "C:\\Users\\OhNo_FR.pcc", "C:\\Users\\OhNo.pcc" },
+                { "C:\\Users\\OhNo_GE.ini", "C:\\Users\\OhNo.ini" },
+            };
+
+            foreach (var v in testData)
+            {
+                Assert.AreEqual(v.Value, v.Key.StripUnrealLocalization(), @"StripUnrealLocalization() returned the wrong localization!");
+            }
+
+        }
+    }
+}
