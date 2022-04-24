938e75084a427e4bb4a202d562e4ae9c42d89c62
Exkywor <fernandoquant.dev@gmail.com>
2022-04-24 12:25:21 -0600

    Add prompt for new ExportID

--- LegendaryExplorer/LegendaryExplorer/Tools/Dialogue Editor/DialogueEditorExperiments/DialogueEditorExperimentsE.cs
+++ LegendaryExplorer/LegendaryExplorer/Tools/Dialogue Editor/DialogueEditorExperiments/DialogueEditorExperimentsE.cs
@@ -1,42 +1,34 @@
-﻿using System.Collections.Generic;
-using System.Windows;
-using LegendaryExplorer.Dialogs;
+﻿using LegendaryExplorer.Dialogs;
 using LegendaryExplorer.UserControls.ExportLoaderControls;
 using LegendaryExplorerCore.Dialogue;
-using LegendaryExplorerCore.Matinee;
 using LegendaryExplorerCore.Packages;
+using System.Collections.Generic;
+using System.Windows;
 
-namespace LegendaryExplorer.DialogueEditor.DialogueEditorExperiments
-{
+namespace LegendaryExplorer.DialogueEditor.DialogueEditorExperiments {
     /// <summary>
     /// Class for Exkywor's preset buttons and stuff
     /// </summary>
-    class DialogueEditorExperimentsE
-    {
+    class DialogueEditorExperimentsE {
         #region Update Native Node String Ref
         // Changes the node's lineref and the parts of the FXA, WwiseStream, and referencing VOs that include it so it doesn't break
-        public static void UpdateNativeNodeStringRef(DialogueEditorWindow dew)
-        {
+        public static void UpdateNativeNodeStringRef(DialogueEditorWindow dew) {
             DialogueNodeExtended selectedDialogueNode = dew.SelectedDialogueNode;
 
-            if (dew.Pcc != null && selectedDialogueNode != null)
-            {
+            if (dew.Pcc != null && selectedDialogueNode != null) {
                 // Need to check if currStringRef exists
                 var currStringRef = selectedDialogueNode.LineStrRef.ToString();
-                if (string.IsNullOrEmpty(currStringRef))
-                {
+                if (string.IsNullOrEmpty(currStringRef)) {
                     MessageBox.Show("The selected node does not have a Line String Ref, which is required in order to programatically replace the required elements.", "Warning", MessageBoxButton.OK);
                     return;
                 }
 
                 var newStringRef = promptForRef("New line string ref:", "Not a valid line string ref.");
-                if (string.IsNullOrEmpty(newStringRef))
-                {
+                if (string.IsNullOrEmpty(newStringRef)) {
                     return;
                 }
 
-                if (currStringRef == newStringRef)
-                {
+                if (currStringRef == newStringRef) {
                     MessageBox.Show("New StringRef matches the existing one.", "Warning", MessageBoxButton.OK);
                     return;
                 }
@@ -59,35 +51,29 @@ namespace LegendaryExplorer.DialogueEditor.DialogueEditorExperiments
             }
         }
 
-        private static void updateVOReferences(IMEPackage pcc, ExportEntry wwiseStream, string oldRef, string newRef)
-        {
-            if (wwiseStream == null)
-            {
+        private static void updateVOReferences(IMEPackage pcc, ExportEntry wwiseStream, string oldRef, string newRef) {
+            if (wwiseStream == null) {
                 return;
             }
 
             var entry = pcc.GetEntry(wwiseStream.UIndex);
 
             var references = entry.GetEntriesThatReferenceThisOne();
-            foreach (KeyValuePair<IEntry, List<string>> reference in references)
-            {
-                if (reference.Key.ClassName != "WwiseEvent")
-                {
+            foreach (KeyValuePair<IEntry, List<string>> reference in references) {
+                if (reference.Key.ClassName != "WwiseEvent") {
                     continue;
                 }
-                ExportEntry refEntry = (ExportEntry) pcc.GetEntry(reference.Key.UIndex);
+                ExportEntry refEntry = (ExportEntry)pcc.GetEntry(reference.Key.UIndex);
                 refEntry.ObjectNameString = refEntry.ObjectNameString.Replace(oldRef, newRef);
             }
 
         }
 
-        private static void updateWwiseStream(ExportEntry wwiseStream, string oldRef, string newRef)
-        {
-            if (wwiseStream is null)
-            {
-               return;
+        private static void updateWwiseStream(ExportEntry wwiseStream, string oldRef, string newRef) {
+            if (wwiseStream is null) {
+                return;
             }
-            
+
             // Pads the string refs so they have the required minimum length
             newRef = newRef.PadLeft(8, '0');
             oldRef = oldRef.PadLeft(8, '0');
@@ -95,35 +81,27 @@ namespace LegendaryExplorer.DialogueEditor.DialogueEditorExperiments
             wwiseStream.ObjectNameString = wwiseStream.ObjectNameString.Replace(oldRef, newRef);
         }
 
-        private static void updateFaceFX(FaceFXAnimSetEditorControl fxa, string oldRef, string newRef)
-        {
-            if (fxa.SelectedLine == null || fxa == null)
-            {
+        private static void updateFaceFX(FaceFXAnimSetEditorControl fxa, string oldRef, string newRef) {
+            if (fxa.SelectedLine == null || fxa == null) {
                 return;
             }
 
             var FaceFX = fxa.FaceFX;
             var SelectedLine = fxa.SelectedLine;
-            
-            if (SelectedLine.Path != null)
-            {
+
+            if (SelectedLine.Path != null) {
                 SelectedLine.Path = SelectedLine.Path.Replace(oldRef, newRef);
             }
-            if (SelectedLine.ID != null)
-            {
+            if (SelectedLine.ID != null) {
                 SelectedLine.ID = newRef;
             }
             // Change FaceFX name
-            if (SelectedLine.NameAsString != null)
-            {
+            if (SelectedLine.NameAsString != null) {
                 string newName = SelectedLine.NameAsString.Replace(oldRef, newRef);
-                if (FaceFX.Names.Contains(newName))
-                {
+                if (FaceFX.Names.Contains(newName)) {
                     SelectedLine.NameIndex = FaceFX.Names.IndexOf(newName);
                     SelectedLine.NameAsString = newName;
-                }
-                else 
-                {
+                } else {
                     FaceFX.Names.Add(newName);
                     SelectedLine.NameIndex = FaceFX.Names.Count - 1;
                     SelectedLine.NameAsString = newName;
@@ -133,13 +111,10 @@ namespace LegendaryExplorer.DialogueEditor.DialogueEditorExperiments
             fxa.SaveChanges();
         }
 
-        private static string promptForRef(string msg, string err)
-        {
-            if (PromptDialog.Prompt(null, msg) is string stringRef)
-            {
+        private static string promptForRef(string msg, string err) {
+            if (PromptDialog.Prompt(null, msg) is string stringRef) {
                 int intRef;
-                if (string.IsNullOrEmpty(stringRef) || !int.TryParse(stringRef, out intRef))
-                {
+                if (string.IsNullOrEmpty(stringRef) || !int.TryParse(stringRef, out intRef)) {
                     MessageBox.Show(err, "Warning", MessageBoxButton.OK);
                     return null;
                 }
@@ -156,7 +131,43 @@ namespace LegendaryExplorer.DialogueEditor.DialogueEditorExperiments
         /// <param name="dew">Dialogue Editor Window instance.</param>
         public static void CloneNodeAndSequence(DialogueEditorWindow dew) {
             MessageBox.Show("Hello", "test", MessageBoxButton.OK);
+
+            DialogueNodeExtended selectedDialogueNode = dew.SelectedDialogueNode;
+
+            if (dew.Pcc != null && selectedDialogueNode != null) {
+                // Need to check if the node has associated data
+                if (selectedDialogueNode.Interpdata == null) {
+                    MessageBox.Show("The selected node does not have an InterpData associated with it.", "Warning", MessageBoxButton.OK);
+                    return;
+                }
+
+                var newID = promptForID("New node ExportID:", "Not a valid ExportID.");
+                if (string.IsNullOrEmpty(newID)) {
+                    return;
+                }
+
+                if (selectedDialogueNode.ExportID.Equals(newID)) {
+                    MessageBox.Show("New ExportID matches the existing one.", "Warning", MessageBoxButton.OK);
+                    return;
+                }
+
+
+
+                MessageBox.Show($"Node cloned and has been given the ExportID {newID}.", "Success", MessageBoxButton.OK);
+            }
         }
+        private static string promptForID(string msg, string err) {
+            if (PromptDialog.Prompt(null, msg) is string strID) {
+                int ID;
+                if (!int.TryParse(strID, out ID)) {
+                    MessageBox.Show(err, "Warning", MessageBoxButton.OK);
+                    return null;
+                }
+                return ID.ToString();
+            }
+            return null;
+        }
+
         #endregion
     }
 }
