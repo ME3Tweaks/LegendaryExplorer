c8cf3b92c9829b69ec38013fd3dd54ca4da22a8a
ME3Tweaks-LEC-Documentation <me3tweaks-auto@me3tweaks.com>
2022-08-19 17:36:11 +0000

    Update documentation [skip ci]

--- nightly/class_legendary_explorer_core_1_1_coalesced_1_1_file_entry.js
+++ nightly/class_legendary_explorer_core_1_1_coalesced_1_1_file_entry.js
@@ -2,5 +2,5 @@ var class_legendary_explorer_core_1_1_coalesced_1_1_file_entry =
 [
     [ "FileEntry", "class_legendary_explorer_core_1_1_coalesced_1_1_file_entry.html#a5472996aad9382f3318c589c78f4a16c", null ],
     [ "Name", "class_legendary_explorer_core_1_1_coalesced_1_1_file_entry.html#a6b6bafd7c64f50b726a2d2d08bbdedc6", null ],
-    [ "Sections", "class_legendary_explorer_core_1_1_coalesced_1_1_file_entry.html#a07d01000dfb165757fd44e6e21a51cbe", null ]
+    [ "Sections", "class_legendary_explorer_core_1_1_coalesced_1_1_file_entry.html#a7c75f0d8380aee2261b7dd20cc1bb16c", null ]
 ];
