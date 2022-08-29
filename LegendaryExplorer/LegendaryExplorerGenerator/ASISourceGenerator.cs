535f5ae322bb70cf96be0331d89cfa09ad765596
Mgamerz <mgamerz2@gmail.com>
2022-08-28 21:33:37 -0600

    Add source generator stub.

---
+++ LegendaryExplorer/LegendaryExplorerGenerator/ASISourceGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace LegendaryExplorerGenerator
+{
+    // This is not hooked up to LEX right now cause it doesn't do anything
+    // If ever implemented, it needs added to the .csproj.
+    // This is the V2 API (.NET 6)
+
+    /// <summary>
+    /// This library is a source generator for Legendary Explorer
+    /// </summary>
+    [Generator]
+    public class LegendaryGenerator : IIncrementalGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // Code generation goes here
+
+            // Todo: Auto update the Interop ASI hashes
+        }
+
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            // Not sure what goes here
+        }
+    }
+}
