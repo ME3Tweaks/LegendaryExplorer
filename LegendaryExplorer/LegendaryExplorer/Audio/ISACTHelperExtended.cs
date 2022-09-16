24067c7a20a664b28a85637e5001ecec0f349b36
Mgamerz <mgamerz@msn.com>
2022-09-15 23:18:20 -0600

    Cleanup ISACT Extended

--- LegendaryExplorer/LegendaryExplorer/Audio/ISACTHelperExtended.cs
+++ LegendaryExplorer/LegendaryExplorer/Audio/ISACTHelperExtended.cs
@@ -12,57 +12,11 @@ namespace LegendaryExplorer.Audio
 {
     internal class ISACTHelperExtended
     {
-        /// <summary>
-        /// Converts an ogg file to a wav file using oggdec
-        /// </summary>
-        /// <param name="oggPath">Path to ogg file</param>
-        /// <returns></returns>
-        public static MemoryStream ConvertOggToWave(string oggPath)
-        {
-            //convert OGG to WAV
-            MemoryStream outputData = new MemoryStream();
-
-            ProcessStartInfo procStartInfo = new ProcessStartInfo(Path.Combine(AppDirectories.ExecFolder, "oggenc2.exe"), $"--stdout \"{oggPath}\"")
-            {
-                WorkingDirectory = AppDirectories.ExecFolder,
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-            //procStartInfo.StandardOutputEncoding = Encoding.GetEncoding(850); //standard cmd-page
-            Process proc = new Process
-            {
-                StartInfo = procStartInfo
-            };
-
-            // Set our event handler to asynchronously read the sort output.
-            proc.Start();
-            //proc.BeginOutputReadLine();
-            var outputTask = Task.Run(() =>
-            {
-                proc.StandardOutput.BaseStream.CopyTo(outputData);
-
-                /*using (var output = new FileStream(outputFile, FileMode.Create))
-                {
-                    process.StandardOutput.BaseStream.CopyTo(output);
-                }*/
-            });
-            Task.WaitAll(outputTask);
-
-            proc.WaitForExit();
-            File.Delete(oggPath); //intermediate
-
-            //Fix headers as they are not correct when output from oggdec over stdout - no idea what it is outputting.
-            outputData.Position = 0x4;
-            outputData.Write(BitConverter.GetBytes(((int)outputData.Length) - 0x8), 0, 4); //filesize
-            outputData.Position = 0x28;
-            outputData.Write(BitConverter.GetBytes(((int)outputData.Length) - 0x24), 0, 4); //datasize
-            outputData.Position = 0;
-            return outputData;
-        }
 
         [DllImport(@"ISACTTools.dll")]
         private static extern int CreateIPSOgg(byte[] wavedata, uint waveDataLen, byte[] dstBuf, uint dstLen);
+
+        // Encodes a .wav file to .ogg
         public static byte[] ConvertWaveToOgg(byte[] wavData)
         {
             byte[] outputBuffer = new byte[wavData.Length]; // It will always be smaller than this
