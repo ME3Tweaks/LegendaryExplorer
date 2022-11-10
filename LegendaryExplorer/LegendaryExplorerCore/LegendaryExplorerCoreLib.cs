830ed15c91b5664615da4b26fd6f2493e08ed8aa
SirCxyrtyx <sircxyrtyx@gmail.com>
2022-11-09 19:32:44 -0800

    LEC: Allow limiting which ObjectInfo DBs are loaded

--- LegendaryExplorer/LegendaryExplorerCore/LegendaryExplorerCoreLib.cs
+++ LegendaryExplorer/LegendaryExplorerCore/LegendaryExplorerCoreLib.cs
@@ -1,6 +1,8 @@
 ﻿using LegendaryExplorerCore.Packages;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using LegendaryExplorerCore.Compression;
 using LegendaryExplorerCore.DebugTools;
@@ -79,7 +81,8 @@ namespace LegendaryExplorerCore
         /// </param>
         /// <param name="packageSavingFailed">Delegate that invoked when a package fails to save</param>
         /// <param name="logger">Serilog logger to use for logging operations. If null, no logging is performed.</param>
-        public static void InitLib(TaskScheduler uiSyncContext, Action<string> packageSavingFailed = null, ILogger logger = null)
+        /// <param name="objectDBsToLoad">Only load object info for specified games. If null, all are loaded.</param>
+        public static void InitLib(TaskScheduler uiSyncContext, Action<string> packageSavingFailed = null, ILogger logger = null, MEGame[] objectDBsToLoad = null)
         {
             if (initialized) return;
             LECLog.logger = logger;
@@ -88,21 +91,24 @@ namespace LegendaryExplorerCore
             MEPackageHandler.Initialize();
             PackageSaver.Initialize();
             PackageSaver.PackageSaveFailedCallback = packageSavingFailed;
-            Action<string>[] jsonLoaders =
+            (MEGame, Action<string>)[] gameToLoaderMap =
             {
-                ME1UnrealObjectInfo.loadfromJSON,
-                ME2UnrealObjectInfo.loadfromJSON,
-                ME3UnrealObjectInfo.loadfromJSON,
-                // Todo: LE Load
-                // Todo: Maybe not load all of these as they use a lot of memory, like 40MB each
-                // For 6 games that will be pretty heavy
-                // Maybe require 
-                // Here for now
-                LE1UnrealObjectInfo.loadfromJSON,
-                LE2UnrealObjectInfo.loadfromJSON,
-                LE3UnrealObjectInfo.loadfromJSON,
-                UDKUnrealObjectInfo.loadfromJSON,
+                (MEGame.ME1, ME1UnrealObjectInfo.loadfromJSON),
+                (MEGame.ME2, ME2UnrealObjectInfo.loadfromJSON),
+                (MEGame.ME3, ME3UnrealObjectInfo.loadfromJSON),
+                (MEGame.LE1, LE1UnrealObjectInfo.loadfromJSON),
+                (MEGame.LE2, LE2UnrealObjectInfo.loadfromJSON),
+                (MEGame.LE3, LE3UnrealObjectInfo.loadfromJSON),
+                (MEGame.UDK, UDKUnrealObjectInfo.loadfromJSON)
             };
+            var jsonLoaders = new List<Action<string>>(gameToLoaderMap.Length);
+            foreach ((MEGame game, Action<string> loader) in gameToLoaderMap)
+            {
+                if (objectDBsToLoad is null || objectDBsToLoad.Contains(game))
+                {
+                    jsonLoaders.Add(loader);
+                }
+            }
             Parallel.ForEach(jsonLoaders, action => action(null));
             if (!OodleHelper.EnsureOodleDll())
             {
