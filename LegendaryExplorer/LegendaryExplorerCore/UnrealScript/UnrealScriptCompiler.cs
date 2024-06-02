48c59574825d0eb67b40baec0b92193583e15767
Mgamerz <mgamerz@msn.com>
2024-06-01 20:37:52 -0600

    Unrealscript Compiler: Change intended class name comparison to be case insensitive to match unreal

--- LegendaryExplorer/LegendaryExplorerCore/UnrealScript/UnrealScriptCompiler.cs
+++ LegendaryExplorer/LegendaryExplorerCore/UnrealScript/UnrealScriptCompiler.cs
@@ -367,7 +367,7 @@ namespace LegendaryExplorerCore.UnrealScript
                     log.LogError("Tried to parse a Class, but no Class was found!");
                     return (null, log);
                 }
-                if (intendedClassName is not null && cls.Name != intendedClassName)
+                if (intendedClassName is not null && !cls.Name.CaseInsensitiveEquals(intendedClassName))
                 {
                     log.LogError($"Class name was '{cls.Name}', expected '{intendedClassName}'!");
                     return (null, log);
