95853fea650e3431f98c2222e0c3996243db30a6
ME3Tweaks-LEC-Documentation <me3tweaks-auto@me3tweaks.com>
2024-06-05 01:58:01 +0000

    Update documentation [skip ci]

--- nightly/class_legendary_explorer_core_1_1_gammtek_1_1_extensions_1_1_collections_1_1_generic_1_1_list_extensions.js
+++ nightly/class_legendary_explorer_core_1_1_gammtek_1_1_extensions_1_1_collections_1_1_generic_1_1_list_extensions.js
@@ -1,4 +1,5 @@
 var class_legendary_explorer_core_1_1_gammtek_1_1_extensions_1_1_collections_1_1_generic_1_1_list_extensions =
 [
-    [ "ReplaceFirstOrAdd< T >", "class_legendary_explorer_core_1_1_gammtek_1_1_extensions_1_1_collections_1_1_generic_1_1_list_extensions.html#a81fa32b378c5994ef0353401c961ac68", null ]
+    [ "ReplaceFirstOrAdd< T >", "class_legendary_explorer_core_1_1_gammtek_1_1_extensions_1_1_collections_1_1_generic_1_1_list_extensions.html#a81fa32b378c5994ef0353401c961ac68", null ],
+    [ "ToCaseInsensitiveDictionary< TOutObjType, TInObjType >", "class_legendary_explorer_core_1_1_gammtek_1_1_extensions_1_1_collections_1_1_generic_1_1_list_extensions.html#addb5e922080728d592599bebce941f29", null ]
 ];
--- nightly/class_legendary_explorer_core_1_1_packages_1_1_m_e_localization_extensions.js
+++ nightly/class_legendary_explorer_core_1_1_packages_1_1_m_e_localization_extensions.js
@@ -2,5 +2,6 @@ var class_legendary_explorer_core_1_1_packages_1_1_m_e_localization_extensions =
 [
     [ "ToLocaleString", "class_legendary_explorer_core_1_1_packages_1_1_m_e_localization_extensions.html#a054fcf0d2062f3b2b3a23f588d8c489c", null ],
     [ "StripUnrealLocalization", "class_legendary_explorer_core_1_1_packages_1_1_m_e_localization_extensions.html#a7f4ef554455f4dd4648dc9e48af3c894", null ],
+    [ "SetUnrealLocalization", "class_legendary_explorer_core_1_1_packages_1_1_m_e_localization_extensions.html#a482c9db7cbb1291600f2dc76ebac0e54", null ],
     [ "GetUnrealLocalization", "class_legendary_explorer_core_1_1_packages_1_1_m_e_localization_extensions.html#a9fbd376ee684a06dcbe9e3683a3120ba", null ]
 ];
