f0d27b77eb159c9d4e754627571eb93682eede6a
Mgamerz <mgamerz@msn.com>
2024-07-20 11:46:42 -0600

    InteropCommands: Begin moving data to constants to help reduce bugs.

---
+++ LegendaryExplorer/LegendaryExplorer/Tools/LiveLevelEditor/InteropCommands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendaryExplorer.Tools.LiveLevelEditor
+{
+    /// <summary>
+    /// Commands for sending to the LEXInterop ASI. Here for consistency.
+    /// </summary>
+    internal static class InteropCommands
+    {
+        public const string INTEROP_LOADPACKAGE = "LOADPACKAGE";
+        public const string INTEROP_SHOWLOADINGINDICATOR = "SHOWLOADINGINDICATOR";
+        public const string INTEROP_HIDELOADINGINDICATOR = "HIDELOADINGINDICATOR";
+
+
+        // LIVE LEVEL EDITOR COMMANDS
+        public const string LLE_SELECT_ACTOR = "LLE_SELECT_ACTOR";
+
+        // LIVE MATERIAL EDITOR COMMANDS
+        public const string LME_SET_SCALAR_EXPRESSION = "LLE_SET_MATEXPR_SCALAR";
+        public const string LME_SET_VECTOR_EXPRESSION = "LLE_SET_MATEXPR_VECTOR";
+        public const string LME_SET_MATERIAL = "LLE_SET_MATERIAL";
+        public const string LME_GET_LOADED_MATERIALS = "LLE_GET_LOADED_MATERIALS";
+    }
+}
